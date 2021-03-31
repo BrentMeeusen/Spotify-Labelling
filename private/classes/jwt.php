4138cb30e4abb37f70d9603a84674ac698e084c6
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-03-31 19:07:55 +0200

    Added createPayload() method

--- private/classes/jwt.php
+++ private/classes/jwt.php
@@ -8,13 +8,16 @@ class JSONWebToken {
 	}
 
 
-
+	private static function createPayload(array $payload) : string {
+		return json_encode($payload);
+	}
 
 
 
 	public static function createToken(array $payload) {
 
 		$header = JSONWebToken::createHeader();
+		$payload = JSONWebToken::createPayload($payload);
 		$signature = "signature";
 
 		return json_encode("$header.$payload.$signature");
