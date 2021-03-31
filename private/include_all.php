73bc5190b2b61c235124a6cb06b4fe210f200c55
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-03-31 18:10:57 +0200

    Added headers here because this file is going to be added to all endpoints

--- private/include_all.php
+++ private/include_all.php
@@ -1,5 +1,9 @@
 <?php
 
+// Headers here because this file is added to all endpoints
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
 // Include all the files here so that all the other files only need one include
 include_once("classes/error.php");
 include_once("classes/label.php");
