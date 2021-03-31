1fb079460d3cda7dfb73b265618372f4970d0bcf
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-03-31 20:24:41 +0200

    Updated test to only run new methods

--- TEST/index.php
+++ TEST/index.php
@@ -5,7 +5,7 @@ include_once("../private/classes/jwt.php");
 
 $token = JSONWebToken::createToken(["sexy" => "yes"], 60);
 $isValid = JSONWebToken::checkToken($token);
-$payload = JSONWebToken::getPayload($token);
+// $payload = JSONWebToken::getPayload($token);
 
 print(json_encode([ "token" => $token, "is_valid" => $isValid, "payload" => $payload ]));
 
