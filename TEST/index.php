9b3c60be5b63f94546f70c85df49863d0fcfab5b
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-03-31 20:49:06 +0200

    Added more tests

--- TEST/index.php
+++ TEST/index.php
@@ -7,6 +7,12 @@ $token = JSONWebToken::createToken(["sexy" => "yes"], 60);
 $isValid = JSONWebToken::checkToken($token);
 // $payload = JSONWebToken::getPayload($token);
 
+
+print("ENCODING CUSTOM");
+print(json_encode(JSONWebToken::checkToken("a.b.YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXos")));
+
+
+print("ENCODING CREATED");
 print(json_encode([ "token" => $token, "is_valid" => $isValid ]));
 // print(json_encode([ "token" => $token, "is_valid" => $isValid, "payload" => $payload ]));
 
