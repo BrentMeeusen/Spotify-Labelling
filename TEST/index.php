92844bbb5e4ee06a736eca3141a174f71e2c0128
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-03-31 20:08:38 +0200

    Added method calls and improved return result:

--- TEST/index.php
+++ TEST/index.php
@@ -3,7 +3,11 @@
 include_once("../private/include_all.php");
 include_once("../private/classes/jwt.php");
 
-print(JSONWebToken::createToken(["sexy" => "yes"], 60));
+$token = JSONWebToken::createToken(["sexy" => "yes"], 60);
+$isValid = JSONWebToken::checkToken($token);
+$payload = JSONWebToken::getPayload($token);
+
+print(json_encode([ "token" => $token, "is_valid" => $isValid, "payload" => $payload ]));
 
 
 ?>
