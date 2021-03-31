996639acf48f6d5667a02bae5d4af34f6977f686
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-03-31 20:02:21 +0200

    Updated javadoc comments layout, added issued_at and expires_at headers

--- private/classes/jwt.php
+++ private/classes/jwt.php
@@ -9,8 +9,9 @@ class JSONWebToken {
 
 	/**
 	 * Takes a string and returns it in a base64 format, where + is -, / is _, and = is omitted
-	 * @param string the string to encode
-	 * @return string the base64 format of the string
+	 * 
+	 * @param 	string	$string		the string to encode
+	 * @return 	string 				the base64 format of the string
 	 */
 	private static function stringToBase64(string $string) : string {
 		return str_replace(["+", "/", "="], ["-", "_", ""], base64_encode($string));
@@ -19,7 +20,8 @@ class JSONWebToken {
 
 	/**
 	 * Creates the header which is the same for every JSON Web Token
-	 * @return string the base64 format of the header
+	 * 
+	 * @return	string	the base64 format of the header
 	 */
 	private static function createHeader() : string {
 		return self::stringToBase64(json_encode([ "typ" => "JWT", "alg" => "HS256" ]));
@@ -28,19 +30,24 @@ class JSONWebToken {
 
 	/**
 	 * Adds the JSON Web Token things to the payload that the user passes
-	 * @param payload to send with the token
-	 * @return string the base64 format of the total payload in JSON format
+	 * 
+	 * @param 	string 	$payload 	to send with the token
+	 * @param 	int 	$mins 		the time in minutes until the token expires
+	 * @return	string				the base64 format of the total payload in JSON format
 	 */
-	private static function createPayload(array $payload) : string {
+	private static function createPayload(array $payload, $mins) : string {
+		$payload["iat"] = time();
+		$payload["exp"] = time() + $mins * 60;
 		return self::stringToBase64(json_encode($payload));
 	}
 
 
 	/**
 	 * Creates the signature based on a SHA256 algorithm
-	 * @param header the header in base64
-	 * @param payload the payload in base64
-	 * @return string the hashed signature in base64
+	 * 
+	 * @param	string	$header		the header in base64
+	 * @param	string	$payload	the payload in base64
+	 * @return	string 				the hashed signature in base64
 	 */
 	private static function createSignature(string $header, string $payload) : string {
 		return self::stringToBase64(hash_hmac("sha256", "$header.$payload", self::$key, true));
@@ -52,8 +59,9 @@ class JSONWebToken {
 
 	/**
 	 * Creates the token using the payload that the user passes
-	 * @param payload the payload to send with the token
-	 * @return string the JSON Web Token
+	 * 
+	 * @param	string	$payload	the payload to send with the token
+	 * @return 	string				the JSON Web Token
 	 */
 	public static function createToken(array $payload) : string {
 
