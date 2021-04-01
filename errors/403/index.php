88d6fe02f8dca32fbaa2aac517cf166a6cf4f352
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-01 20:40:35 +0200

    Added exit after error message

--- errors/403/index.php
+++ errors/403/index.php
@@ -1,4 +1,5 @@
 <?php
 include_once("../../private/include_all.php");
 print(json_encode(httpResponseCode(403, "Forbidden")));
+exit();
 ?>
--- errors/404/index.php
+++ errors/404/index.php
@@ -1,4 +1,5 @@
 <?php
 include_once("../../private/include_all.php");
 print(json_encode(httpResponseCode(404, "Not Found")));
+exit();
 ?>
