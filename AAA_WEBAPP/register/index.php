1f0994d6a2dd0f3c5283b68e9a6dcf06c8124d1d
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-02 18:35:28 +0200

    Added registration form structure

--- AAA_WEBAPP/register/index.php
+++ AAA_WEBAPP/register/index.php
@@ -18,6 +18,19 @@
 	<body>
 
 		<h1>Spotify Labelling</h1>
+
+		<div class="form register-form">
+
+			<input name="first-name"		type="text"			placeholder="FIRST NAME">
+			<input name="last-name"			type="text"			placeholder="LAST NAME">
+			<input name="username"			type="text"			placeholder="USERNAME">
+			<input name="email-address"		type="text"			placeholder="EMAIL ADDRESS">
+			<input name="password"			type="password"		placeholder="PASSWORD">
+			<input name="password-repeat"	type="password"		placeholder="REPEAT PASSWORD">
+
+			<button>REGISTER</button>
+
+		</div>
 		
 	</body>
 </html>
