86d52648b1ec27b259db4cdd9f19acc2a66b92ac
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-03 03:31:21 +0200

    Fixed cookie issues

--- AAA_API/jwt/generate/index.php
+++ AAA_API/jwt/generate/index.php
@@ -1,7 +1,12 @@
 <?php
 
 include_once("../../private/include_all.php");
+
+// Create token
 $token = JSONWebToken::createToken(["login" => TRUE, "register" => TRUE], 15);
-setcookie("jwt", $token, time() + 3600, "/", "", false, true);
+
+// Clear cookie and store new cookie
+setcookie("jwt", "", time() - 60);
+setcookie("jwt", $token, time() + 15, "/", "", false, true);
 
 ?>
