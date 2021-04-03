b83c729cda34dc76192c1ca6f54674fa8f02bb88
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-03 03:28:17 +0200

    Created cookie

--- AAA_API/jwt/generate/index.php
+++ AAA_API/jwt/generate/index.php
@@ -2,10 +2,6 @@
 
 include_once("../../private/include_all.php");
 $token = JSONWebToken::createToken(["login" => TRUE, "register" => TRUE], 15);
-$token = json_encode(["jwt" => $token]);
-
-print($token);
-
-header( "Set-Cookie: jwt=$token; httpOnly" );
+setcookie("jwt", $token, time() + 3600, "/", "", false, true);
 
 ?>
