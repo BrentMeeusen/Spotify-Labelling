8750695490422bbff86dcc2954c09a250dfc59db
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-03 02:01:16 +0200

    Created HTML-JavaScript form JS handling

--- AAA_WEBAPP/assets/js/general.js
+++ AAA_WEBAPP/assets/js/general.js
@@ -1,14 +1,70 @@
-let theme = "dark";
+let _THEME = "dark";
 
 function changeTheme() {
 
 	const variables = ["background", "text", "placeholder", "link", "background-highlight", "highlight"];
-
-	theme = (theme === "dark" ? "light" : "dark");
+	_THEME = (_THEME === "dark" ? "light" : "dark");
 
 	for(let i = 0 ; i < variables.length; i++) {
 		document.documentElement.style.setProperty("--current--" + variables[i], 
-				getComputedStyle(document.documentElement).getPropertyValue("--" + theme + "--" + variables[i]));
+				getComputedStyle(document.documentElement).getPropertyValue("--" + _THEME + "--" + variables[i]));
 	}
 
 }
+
+
+
+/**
+ * When the website loads
+ */
+window.addEventListener("load", () => {
+
+	// Load all the HTML/JavaScript
+	const forms = document.getElementsByName("html-js-form");
+	let index = 0;
+
+	// For each form
+	for(f of forms) {
+
+		// When the corresponding submit button is clicked
+		const submit = document.getElementsByName("html-js-form-submit")[index++];
+		submit.addEventListener("click", () => {
+
+			// Get all inputs and values
+			let inputs = [];
+			for(i of f.childNodes) {
+				if(i.name) {
+					if(i.name.includes("input")) {
+						inputs.push({ name: i.name.replace("input ", ""), value: i.value });
+					}
+				}
+			}			
+
+			// Create an XMLHttpResponse
+			requestLabellingApiEndpoint(f.dataset.action, f.dataset.method, inputs);
+
+		});
+		
+	}
+	
+});
+
+
+
+
+
+/**
+ * Requests an endpoint from the Spotify Labelling API
+ * 
+ * @param {string} action 
+ * @param {string} method 
+ * @param {array[string]} values 
+ */
+function requestLabellingApiEndpoint(action, method, values) {
+	console.log("action", action);
+	console.log("method", method);
+	console.log("values", values);
+}
+
+
+
