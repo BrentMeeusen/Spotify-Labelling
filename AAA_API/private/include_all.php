56c975c6528653e58c3773fb9439c9bc526ab690
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-07 20:57:18 +0200

    Renamed error file to api-response and edited it in the include_all

--- AAA_API/private/include_all.php
+++ AAA_API/private/include_all.php
@@ -5,8 +5,8 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 // Include all the files here so that all the other files only need one include
+include_once("classes/api-response.php");
 include_once("classes/database.php");
-include_once("classes/error.php");
 include_once("classes/jwt.php");
 include_once("classes/label.php");
 
