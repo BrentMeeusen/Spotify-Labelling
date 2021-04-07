63a0db459a621683f9d2d482bcf15aa8a60f0a4d
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-07 20:59:15 +0200

    Returned JWT token so the client can store it and send it in the authorisation

--- AAA_API/jwt/generate/index.php
+++ AAA_API/jwt/generate/index.php
@@ -9,4 +9,7 @@ $token = JSONWebToken::createToken(["login" => TRUE, "register" => TRUE], 15);
 setcookie("jwt", "", time() - 60);
 setcookie("jwt", $token, time() + 15, "/", "", false, true);
 
+// Return the cookie so that the client can store it and send it on a request
+ApiResponse::httpResponse(200, ["jwt" => $token]);
+
 ?>
