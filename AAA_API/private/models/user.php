06c6ada5c42b13b00d31b0df91ec1a753a93cdb6
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-09 17:57:31 +0200

    Added accountstatus method, createuser method

--- AAA_API/private/models/user.php
+++ AAA_API/private/models/user.php
@@ -4,13 +4,37 @@ class User {
 
 	// Initialise variables
 	private mysqli $conn;
+
 	private int $id;
 	private string $firstName;
 	private string $lastName;
-	private string $username;
 	private string $emailAddress;
+
+	private string $username;
+	private string $password;
+
 	private int $accountStatus;
 	private string $accountStatusText;
+
+
+
+
+	/**
+	 * 
+	 */
+	private function setAccountStatus(int $status) : void {
+		
+		$this->accountStatus = $status;
+		switch($status) {
+			case 1:
+				$accountStatusText = "Created";
+				break;
+			case 2:
+				$accountStatusText = "Verified";
+				break;
+		}
+
+	}
 
 
 
@@ -18,12 +42,33 @@ class User {
 
 	/**
 	 * User constructor
-	 * @param mysqli connection
+	 * @param		mysqli		connection
 	 */
 	public function __construct(mysqli $conn) {
 		$this->conn = $conn;
 	}
 
+
+
+
+
+	/**
+	 * Create the user with the given values
+	 * @param		array		the values to create the user with
+	 * @return		bool		whether it was successful or not
+	 */
+	public function createUser(array $values) : bool {
+
+		$firstName = $values["FirstName"];
+		$lastName = $values["LastName"];
+		$username = $values["Username"];
+		$password = $values["Password"];
+		$emailAddress = $values["EmailAddress"];
+		$this->setAccountStatus(1);
+
+		return FALSE;
+
+	}
 
 
 }
