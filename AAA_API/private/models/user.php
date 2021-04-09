4aed73fd1fbec9987550f49f5e6f9123ebbb548e
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-09 17:25:28 +0200

    Initialised user variables

---
+++ AAA_API/private/models/user.php
@@ -0,0 +1,21 @@
+<?php
+
+class User {
+
+	// Initialise variables
+	private mysqli $conn;
+	private int $id;
+	private string $firstName;
+	private string $lastName;
+	private string $username;
+	private string $emailAddress;
+	private int $accountStatus;
+
+
+
+}
+
+
+
+
+?>
