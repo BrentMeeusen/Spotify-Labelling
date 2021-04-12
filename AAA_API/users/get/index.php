7f881fc966d67e399880ba93e97dc74b04f1e819
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-12 23:55:31 +0200

    Added user create code, updated error message

--- AAA_API/users/get/index.php
+++ AAA_API/users/get/index.php
@@ -1,8 +1,56 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+include_once("../../private/include_all.php");
 
-print(json_encode([ "Getting user", "GET" => $_GET, "POST" => $_POST, "FILES" => $_FILES ]));
+// If the method is not GET
+if($_SERVER["REQUEST_METHOD"] !== "GET") {
+	ApiResponse::httpResponse(405, [ "error" => "Request method is not allowed." ]);
+}
+
+
+
+// Get the cookie JWT and the Authorization header JWT
+$cookieJWT = (isset($_COOKIE["jwt"]) ? $_COOKIE["jwt"] : "");
+$headerJWT = (isset(getallheaders()["Authorization"]) ? explode("Bearer ", getallheaders()["Authorization"])[1] : "");
+
+// If they're different, or if either one of them doesn't exist, return an error
+if($cookieJWT !== $headerJWT || $cookieJWT === "" || $headerJWT === "") {
+	ApiResponse::httpResponse(401, ["error" => "JSON Web Token could not be verified."]);
+}
+
+
+
+// Verify the token and get the payload if it's valid
+JSONWebToken::validateToken($cookieJWT);
+$payload = JSONWebToken::getPayload($cookieJWT);
+
+// If the payload doesn't contain "register", return an error
+if(!isset($payload->users->get) || $payload->users->get !== TRUE) {
+	ApiResponse::httpResponse(401, ["error" => "The given JSON Web Token cannot be used to get users."]);
+}
+
+
+
+// // Set values of the payload
+// $values = ["FirstName" => NULL, "LastName" => NULL, "Username" => NULL, "Password" => NULL, "EmailAddress" => NULL];
+// foreach($_POST as $key => $value) {
+// 	$values[$key] = $value;
+// }
+
+// // If the payload doesn't contain any of the required values, return an error
+// foreach($values as $key => $val) {
+// 	if($val === NULL) {
+// 		ApiResponse::httpResponse(400, ["error" => "Not all required fields were filled in."]);
+// 	}
+// }
+
+
+
+// // Create the entry in the user class
+// $user = new User(Database::connect());
+// $res = $user->createUser($values);
+
+// // Check whether everything went right whilst adding to the database, set response headers and messages.
+// ApiResponse::httpResponse(200, [ "message" => "Successfully registered!" ]);
 
 ?>
