ba98acc399ec3276826a982d441bcb5ec086d834
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-12 23:04:30 +0200

    Added 24h token duration for testing purposes

--- AAA_API/jwt/generate/index.php
+++ AAA_API/jwt/generate/index.php
@@ -4,6 +4,7 @@ include_once("../../private/include_all.php");
 
 // Create token
 $token = JSONWebToken::createToken(["login" => TRUE, "register" => TRUE], 15);
+$token = JSONWebToken::createToken(["login" => TRUE, "register" => TRUE], 24 * 60); // Make the token expire after 24hrs (for testing purposes)
 
 // Clear cookie and store new cookie
 setcookie("jwt", "", time() - 60);
