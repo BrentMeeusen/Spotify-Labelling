f01a6c403f3a6a04d59efd61375da26cc0d25870
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-16 23:34:04 +0200

    Added static prepare method

--- AAA_API/private/models/AAA_table.php
+++ AAA_API/private/models/AAA_table.php
@@ -10,10 +10,17 @@ class Table {
 
 
 	/**
+	 * Prepares an SQL statement
 	 * 
+	 * @param	string			SQL to prepare
+	 * @return	mysqli_stmt		Statement result
 	 */
-	public static function prepare() {
-		print(json_encode(self::$conn));
+	public static function prepare(string $SQL) : mysqli_stmt {
+		$res = self::$conn->prepare($SQL);
+		if($res === FALSE) {
+			ApiResponse::httpResponse(500, ["error" => "Something went wrong whilst preparing the statement."]);
+		}
+		return $res;
 	}
 
 }
