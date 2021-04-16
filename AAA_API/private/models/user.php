0dbfca1faa8d0d2de3f78471b4c7248e21ffdb91
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-16 23:38:02 +0200

    Added parameter to the result

--- AAA_API/private/models/user.php
+++ AAA_API/private/models/user.php
@@ -358,7 +358,7 @@ class User extends Table {
 		$stmt = Table::prepare("SELECT * FROM USERS WHERE EmailAddress = ?;");
 		$email = Table::sanitizeArray([$emailAddress])[0];
 		$stmt->bind_param("s", $email);
-		$res = Table::getResult();
+		$res = Table::getResult($stmt);
 
 		print(json_encode(["STMT" => $stmt, "RESULT" => $res]));
 
