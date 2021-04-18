dceffa1d3e4ac9f80b0c83ad55dd4623d82c7bc2
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-18 13:04:53 +0200

    Removed initial code

--- AAA_API/users/edit/index.php
+++ AAA_API/users/edit/index.php
@@ -2,9 +2,4 @@
 
 include_once("../../private/include_all.php");
 
-
-$msg = httpResponseCode(200, "OK");
-
-print(json_encode([ $msg, "editing user", "GET" => $_GET, "POST" => $_POST, "FILES" => $_FILES ]));
-
 ?>
