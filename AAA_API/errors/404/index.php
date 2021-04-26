45513cf3d0ee6d108cb2cae4ed85f4ee51455874
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-26 23:31:35 +0200

    Added URI and URL in 404

--- AAA_API/errors/404/index.php
+++ AAA_API/errors/404/index.php
@@ -1,4 +1,4 @@
 <?php
 include_once("../../private/include_all.php");
-ApiResponse::httpResponse(404, [ "error" => "Not Found" ]);
+ApiResponse::httpResponse(404, [ "error" => "Not Found", "URI" => $_SERVER['REQUEST_URI'], "URL" => $_SERVER['REDIRECT_URL'] ]);
 ?>
