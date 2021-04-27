b5aff794075a3f58086a3e862a85246cf0503f65
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-04-27 14:10:21 +0200

    Fixed JS bug

--- AAA_WEBAPP/assets/js/general.js
+++ AAA_WEBAPP/assets/js/general.js
@@ -54,11 +54,11 @@ window.addEventListener("load", () => {
 	// DARK THEME ===================================================================
 	// Set the variables to update
 	const variables = ["background", "text", "placeholder", "link", "background-highlight", "highlight"];
-	themeButton.dataset.theme = (themeButton.dataset.theme === "dark" ? "light" : "dark");
+	const theme = "dark"; // Will be set as a cookie!
 
 	// Loop over all variables and change their values
 	for(let i = 0 ; i < variables.length; i++) {
-		document.documentElement.style.setProperty("--current--" + variables[i], getComputedStyle(document.documentElement).getPropertyValue("--" + themeButton.dataset.theme + "--" + variables[i]));
+		document.documentElement.style.setProperty("--current--" + variables[i], getComputedStyle(document.documentElement).getPropertyValue("--" + theme + "--" + variables[i]));
 	}
 	
 });
