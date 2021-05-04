a89c3b636c5ec0c0d9e3516245ca1d8712175a51
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-05 01:31:06 +0200

    Updated all scripts and comments, reordered them

--- AAA_WEBAPP/register/index.php
+++ AAA_WEBAPP/register/index.php
@@ -49,12 +49,16 @@
 		</div>
 
 
+		<!-- Load all files -->
 		<script src="../assets/js/api.js"></script>
-		<script src="../assets/js/lazy-loading.js"></script>
 		<script src="../assets/js/html-js-form.js"></script>
+		<script src="../assets/js/lazy-loading.js"></script>
 		<script src="../assets/js/popup.js"></script>
-		<script src="../assets/js/general.js"></script>
 		<script src="../assets/js/register.js"></script>
+		<script src="../assets/js/theme.js"></script>
+
+		<!-- Load general file (which will be a minified version of all the files above that are general) -->
+		<script src="../assets/js/general.js"></script>
 		
 	</body>
 </html>
