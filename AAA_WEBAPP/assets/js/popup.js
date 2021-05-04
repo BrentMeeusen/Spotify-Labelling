b94fd4e492d3afc7d7f5be58d3bfd9cdac6099a7
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-04 23:31:19 +0200

    Moved popup code to a separate file

---
+++ AAA_WEBAPP/assets/js/popup.js
@@ -0,0 +1,23 @@
+class Popup {}
+Popup.container = document.getElementById("popup");
+Popup.text = document.getElementById("popup-text");
+
+/**
+ * Show the popup
+ * 
+ * @param {string} message The message to show in the popup
+ * @param {string} type The type of the message
+ * @param {string} dur The duration of the popup
+ */
+Popup.show = (message, type, dur) => {
+
+	Popup.text.innerHTML = message;
+	Popup.container.style.top = 0;
+	Popup.container.style.borderBottomColor = getComputedStyle(document.documentElement).getPropertyValue("--current--" + type);
+
+	clearTimeout(Popup.timeout);
+	Popup.timeout = setTimeout(() => {
+		Popup.container.style.top = (-1 * Popup.container.offsetHeight) - 10 + "px";
+	}, dur);
+
+}
