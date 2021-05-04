a9fe6c3596530d6d8b1c875e13ff2c7e3fe70eb4
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-05 01:00:24 +0200

    Added html-js-form.js

--- AAA_WEBAPP/assets/js/html-js-form.js
+++ AAA_WEBAPP/assets/js/html-js-form.js
@@ -1,24 +1,12 @@
-var IMAGES = [];
-var TOKEN;
+class HtmlJsForm {
 
-/**
- * When the website loads
- */
-window.addEventListener("load", async () => {
-
-
-	// REFACTORING ====
-
-
-	const lazy = new LazyLoading("lazy-image");
-	const forms = HtmlJsForm.getForms();
-
-
-	// END REFACTORING ==== 
+}
 
 
 
-	// HTML JAVASCRIPT FORMS ===================================================================
+
+
+/*
 	// Load all the HTML/JavaScript
 	const forms = document.getElementsByName("html-js-form");
 	let index = 0;
@@ -58,18 +46,4 @@ window.addEventListener("load", async () => {
 		
 	}
 
-
-	// DARK THEME ===================================================================
-	// Set the variables to update
-	const variables = ["background", "text", "placeholder", "link", "background-highlight", "highlight"];
-	const theme = "dark"; // Will be set as a cookie!
-
-	// Loop over all variables and change their values
-	for(let i = 0 ; i < variables.length; i++) {
-		document.documentElement.style.setProperty("--current--" + variables[i], getComputedStyle(document.documentElement).getPropertyValue("--" + theme + "--" + variables[i]));
-	}
-	
-});
-
-
-
+*/
