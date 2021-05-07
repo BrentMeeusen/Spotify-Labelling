8ff21afae3f5e96a54be72b377907d505945ec94
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-07 11:12:45 +0200

    Added database connection for the online version

--- AAA_API/private/classes/database.php
+++ AAA_API/private/classes/database.php
@@ -11,6 +11,15 @@ class Database {
 	private static mysqli $conn;
 
 
+	/*
+	private static string $host = "localhost";
+	private static string $username = "u236549530_BrentSpotify";
+	private static string $password = "MySp0t1fy!";
+	private static string $database = "u236549530_SpotifyLabels";
+	private static mysqli $conn;
+	*/
+
+
 
 
 
