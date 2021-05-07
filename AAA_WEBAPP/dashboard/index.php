eda4e24a945e05515d4f94c2ab27cb8b4802e0d1
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-07 10:14:24 +0200

    Updated links to match files

--- AAA_WEBAPP/dashboard/index.php
+++ AAA_WEBAPP/dashboard/index.php
@@ -13,7 +13,7 @@ session_start();
 		
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap">
-		<link rel="stylesheet" type="text/css" href="assets/css/general.css">
+		<link rel="stylesheet" type="text/css" href="../assets/css/general.css">
 	</head>
 
 
@@ -47,17 +47,17 @@ session_start();
 
 
 		<!-- Load all files -->
-		<script src="assets/js/api.js"></script>
-		<script src="assets/js/html-js-form.js"></script>
-		<script src="assets/js/lazy-loading.js"></script>
-		<script src="assets/js/popup.js"></script>
-		<script src="assets/js/theme.js"></script>
+		<script src="../assets/js/api.js"></script>
+		<script src="../assets/js/html-js-form.js"></script>
+		<script src="../assets/js/lazy-loading.js"></script>
+		<script src="../assets/js/popup.js"></script>
+		<script src="../assets/js/theme.js"></script>
 
 		<!-- Load general file (which will be a minified version of all the files above that are general) -->
-		<script src="assets/js/general.js"></script>
+		<script src="../assets/js/general.js"></script>
 
 		<!-- Load code for showing result of the action of the previous page (due to redirect) -->
-		<?php include_once("assets/php/show-redirect-result.php"); ?>
+		<?php include_once("../assets/php/show-redirect-result.php"); ?>
 
 	</body>
 </html>
