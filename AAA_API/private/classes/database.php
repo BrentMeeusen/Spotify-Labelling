2aa2bdcc254bfe7aa211eff3887ed75e08a4f2ec
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-07 16:53:35 +0200

    Updated table scheme

--- AAA_API/private/classes/database.php
+++ AAA_API/private/classes/database.php
@@ -91,6 +91,7 @@ class Database {
 		$tableName = "USERS";
 		$SQL = "CREATE TABLE $tableName (
 			ID				INT 			NOT NULL 	AUTO_INCREMENT,
+			PublicID		VARCHAR(32)		NOT NULL,
 			FirstName		VARCHAR(50) 	NOT NULL,
 			LastName		VARCHAR(50)		NOT NULL,
 			Username		VARCHAR(100)	NOT NULL,
