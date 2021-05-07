89c101615d7dc1e1d191856a8b02bf79526442d8
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-07 16:58:42 +0200

    Updated file paths

--- AAA_WEBAPP/assets/snippets/javascript-files.php
+++ AAA_WEBAPP/assets/snippets/javascript-files.php
@@ -1,10 +1,10 @@
 <!-- Load all files -->
-<script src="assets/js/api.js"></script>
-<script src="assets/js/html-js-form.js"></script>
-<script src="assets/js/jwt.js"></script>
-<script src="assets/js/lazy-loading.js"></script>
-<script src="assets/js/popup.js"></script>
-<script src="assets/js/theme.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/api.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/html-js-form.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/jwt.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/lazy-loading.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/popup.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/theme.js"></script>
 
 <!-- Load general file (which will be a minified version of all the files above that are general) -->
-<script src="assets/js/general.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/general.js"></script>
