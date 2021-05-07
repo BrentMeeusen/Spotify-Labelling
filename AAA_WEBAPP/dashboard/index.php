9c8ddb0f4844a1dcb9c59295abd3c15627cb8ba3
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-07 16:39:09 +0200

    Updated include_once paths

--- AAA_WEBAPP/dashboard/index.php
+++ AAA_WEBAPP/dashboard/index.php
@@ -55,10 +55,10 @@ session_start();
 
 		<?php
 		// Load JavaScript
-		include_once("assets/snippets/javascript-files.php"); 
+		include_once("../assets/snippets/javascript-files.php");
 
 		// Load showing result after redirect
-		include_once("assets/snippets/show-redirect-result.php");
+		include_once("../assets/snippets/show-redirect-result.php");
 		?>
 
 	</body>
