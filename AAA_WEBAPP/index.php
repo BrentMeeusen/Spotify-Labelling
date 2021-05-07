bba862d751db34b0bffa9a6b2fec97231fafcf03
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-07 09:44:38 +0200

    Added CSS and JS files

--- AAA_WEBAPP/index.php
+++ AAA_WEBAPP/index.php
@@ -10,6 +10,10 @@ session_start();
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Spotify Labelling</title>
+		
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap">
+		<link rel="stylesheet" type="text/css" href="../assets/css/general.css">
 	</head>
 
 
@@ -25,6 +29,18 @@ session_start();
 			?>
 		
 		</div>
+
+
+		<!-- Load all files -->
+		<script src="../assets/js/api.js"></script>
+		<script src="../assets/js/html-js-form.js"></script>
+		<script src="../assets/js/lazy-loading.js"></script>
+		<script src="../assets/js/popup.js"></script>
+		<script src="../assets/js/register.js"></script>
+		<script src="../assets/js/theme.js"></script>
+
+		<!-- Load general file (which will be a minified version of all the files above that are general) -->
+		<script src="../assets/js/general.js"></script>
 
 	</body>
 </html>
