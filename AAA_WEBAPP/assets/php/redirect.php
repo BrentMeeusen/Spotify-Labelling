f2877b343a258891fb6418a01b80872d1bc004e4
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-08 12:14:48 +0200

    Set cookie on client side whenever a JWT is received

--- AAA_WEBAPP/assets/php/redirect.php
+++ AAA_WEBAPP/assets/php/redirect.php
@@ -3,10 +3,20 @@
 session_start();
 $redirect = "../../" . $_GET["redirect"];
 
+// Get the code and message
 $_SESSION["code"] = $_GET["code"];
 $_SESSION["message"] = $_GET["message"];
 
-if(!empty($_GET["jwt"]) && $_GET["jwt"] != "undefined") { $_SESSION["jwt"] = $_GET["jwt"]; }
+// If the API responded with a JSON Web Token
+if(!empty($_GET["jwt"]) && $_GET["jwt"] != "undefined") {
+	
+	// Set the token in the session and create a cookie for it
+	$_SESSION["jwt"] = $_GET["jwt"];
+	setcookie("jwt", "", 1, "/");
+	setcookie("jwt", $_GET["jwt"], time() + (60 * 60), "/", "", FALSE, TRUE);
+
+
+}
 
 header("Location: $redirect");
 exit();
