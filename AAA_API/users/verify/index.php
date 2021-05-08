36b5e40963888f94393a3c9237d622429f810170
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-08 14:02:19 +0200

    Added endpoint for verifying an account

---
+++ AAA_API/users/verify/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$ALLOWED_METHOD = "GET";
+
+include_once("../../private/include_all.php");
+
+
+
+// Get the user by ID
+if(!isset($_GET["id"]) || !isset($_GET["email"])) {
+	ApiResponse::httpResponse(400, ["error" => "Not all required fields were passed."]);
+}
+
+// Verify the user
+$user = User::findByPublicID($_GET["id"]);
+if($user === NULL || $user->email === $_GET["email"]) {
+	ApiResponse::httpResponse(400, ["error" => "The requested user wasn't found."]);
+}
+
+// Set the account status
+$user->verify();
+
+// Properly return the results
+ApiResponse::httpResponse(200, ["message" => "Successfully verified account."]);
+
+
+?>
