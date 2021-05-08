6c50ccf21c3ad83bf4e8354d30f8aa7d62d49b19
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-08 14:50:24 +0200

    Fixed redirecting bug

--- AAA_WEBAPP/verify-account/index.php
+++ AAA_WEBAPP/verify-account/index.php
@@ -2,8 +2,14 @@
 
 // WARNING: HARDCODED LOCATION; CHANGE TO "http://spotify-labelling-api.21webb.nl/api/v1/users/verify"
 $url = "http://localhost/Spotify%20Labelling/AAA_API/api/v1/users/verify/" . $_GET["id"] . "/" . $_GET["email"];
-$res = json_decode(file_get_contents($url));
-header("Location: ../assets/php/redirect.php?code=" . $res->code . "&message=" . $res->message . "&redirect=");
+$res = json_decode(@file_get_contents($url));
+
+if($res === NULL) {
+	@$res->code = 400;
+	$res->message = "Something went wrong whilst verifying the account.";
+}
+
+header("Location: ../assets/php/redirect.php?code=" . $res->code . "&message=" . (isset($res->message) ? $res->message : $res->error) . "&redirect=");
 
 ?>
 
