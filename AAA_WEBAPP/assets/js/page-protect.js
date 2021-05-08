95a66a6fb4a4ef8f78d52ec2695c261f39f9f078
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-08 13:03:31 +0200

    Added additional code for page protection

--- AAA_WEBAPP/assets/js/page-protect.js
+++ AAA_WEBAPP/assets/js/page-protect.js
@@ -11,13 +11,13 @@ class PageProtect {}
  */
 PageProtect.protect = (options) => {
 
-	// Get the token
-	const payload = Api.TOKEN.getPayload();
-
-	// If the token is invalid, redirect to login
-	if(!payload) {
+	// If there's no token, or if the token is invalid, redirect to login
+	if(!Api.TOKEN || !Api.TOKEN.validate()) {
 		window.location.href = "/Spotify Labelling/AAA_WEBAPP/assets/php/redirect.php?code=403&message=" + encodeURIComponent("Access forbidden.") + "&redirect=" + encodeURIComponent("");
 	}
+	
+	// Get the token
+	const payload = Api.TOKEN.getPayload();
 
 	// If the user needs to be verified AND if the account status is less than the required level, redirect to login 
 	if(options.verifiedLevel && payload.user.accountStatus < options.verifiedLevel) {
