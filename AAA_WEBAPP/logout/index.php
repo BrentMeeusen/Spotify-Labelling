60d7d8f60d7a201dd48d5343f5daf01f1af7c61b
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-09 14:03:42 +0200

    Added logout functionality

--- AAA_WEBAPP/logout/index.php
+++ AAA_WEBAPP/logout/index.php
@@ -1,3 +1,15 @@
 <?php
 
+// Unset cookie
+setcookie("jwt", "", 1, "/");
+
+// Set variables
+if(!isset($_GET["code"])) { $_GET["code"] = 200; }
+if(!isset($_GET["message"])) { $_GET["message"] = "You have been successfully logged out."; }
+$_GET["redirect"] = "";
+
+// Redirect to login
+include_once("../assets/php/redirect.php");
+
+
 ?>
