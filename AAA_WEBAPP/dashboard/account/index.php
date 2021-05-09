d3583147ee6cb4bd6bbfa51507cd7f436f11f8e3
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-09 13:50:31 +0200

    Added redirect to logout on success

--- AAA_WEBAPP/dashboard/account/index.php
+++ AAA_WEBAPP/dashboard/account/index.php
@@ -65,7 +65,7 @@ session_start();
 			
 			<div class="module">
 
-				<div class="form register-form" name="html-js-form" data-action="api/v1/users/delete" data-method="DELETE">
+				<div class="form register-form" name="html-js-form" data-action="api/v1/users/delete" data-method="DELETE" data-redirect="logout">
 
 					<button class="border--red" type="submit" name="html-js-form-submit" value="submit" id="update-password-btn">DELETE ACCOUNT</button>
 
