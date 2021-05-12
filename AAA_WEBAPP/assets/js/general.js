2271c04e363af25b9d7bc554718a0663e9cc70d2
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-12 09:16:50 +0200

    Added navigation instance

--- AAA_WEBAPP/assets/js/general.js
+++ AAA_WEBAPP/assets/js/general.js
@@ -7,4 +7,13 @@ window.addEventListener("load", async () => {
 	const lazy = new LazyLoading("lazy-image");
 	Theme.setTheme("dark");
 
+	// Create menu object
+	const nav = document.getElementById("nav-menu");
+	const open = document.getElementById("nav-open");
+	const close = document.getElementById("nav-close");
+
+	if(nav && open && close) {
+		const navigation = new Navigation(nav, open, close);
+	}
+
 });
