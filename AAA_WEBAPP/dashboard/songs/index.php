aca5393c0c521664e61891b52ce691b8f2fedf37
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-12 09:52:56 +0200

    Added song page

---
+++ AAA_WEBAPP/dashboard/songs/index.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Spotify Labelling | Your Account</title>
+		
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap">
+		<link rel="stylesheet" type="text/css" href="../../assets/css/general.css">
+		<link rel="stylesheet" type="text/css" href="../../assets/css/dashboard.css">
+	</head>
+
+
+	<body>
+
+		<div class="main-wrapper">
+
+			<!-- Popup -->
+			<div class="popup" id="popup">
+				<p id="popup-text"></p>
+			</div>
+
+			<!-- Menu -->
+			<div class="nav-btn" id="nav-open">
+				<img src="../../assets/icons/menu/menu-open.png">
+			</div>
+
+			<?php include_once("../../assets/snippets/navigation.php"); ?>
+
+			<!-- Title -->
+			<h1><a href="">Spotify Labelling</a></h1>
+			
+			<p>SONGS GO HERE</p>
+
+		</div>	<!-- .main-wrapper -->
+
+
+
+		<?php
+		// Load JavaScript
+		include_once("../../assets/snippets/javascript-files.php");
+		?>
+
+		<script>
+
+		// Protect the page
+		PageProtect.protect({ verifiedLevel: 2 });
+
+		</script>
+
+	</body>
+</html>
