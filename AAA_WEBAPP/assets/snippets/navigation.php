c7bb9a0de191dc19216756b46463d6eb071a586e
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-12 09:07:59 +0200

    Added closing button to the menu

--- AAA_WEBAPP/assets/snippets/navigation.php
+++ AAA_WEBAPP/assets/snippets/navigation.php
@@ -1,4 +1,8 @@
-<div class="nav">
+<div class="nav" id="nav-menu">
+	<div class="nav-btn" id="nav-close">
+		<img src="/Spotify Labelling/AAA_WEBAPP/assets/icons/menu/menu-close.png">
+	</div>
+	
 	<ul>
 		<li><a href="account">Your Account</a></li>
 	</ul>
