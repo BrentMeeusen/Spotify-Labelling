a187ebcc032976911a8fb5c96cbb184e6aec5fdd
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-12 10:23:51 +0200

    Added method

--- AAA_API/private/methods.php
+++ AAA_API/private/methods.php
@@ -2,4 +2,18 @@
 
 
 
+/**
+ * Checks whether a key in an array is set AND is empty
+ * 
+ * @param	array	The array to search in
+ * @param	string	The key of the array
+ * @return	bool	True if it's set and empty, false if it's not
+ */
+function setAndEmpty($array, $key) : bool {
+	if(isset($array[$key]) && empty($array[$key])) {
+		return TRUE;
+	}
+	return FALSE;
+}
+
 ?>
