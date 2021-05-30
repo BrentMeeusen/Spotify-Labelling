f419c2c949b7bb13231d978a826dbc1a9fe9aa0d
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-30 15:21:39 +0200

    Added variables, constructor

--- AAA_API/private/models/label.php
+++ AAA_API/private/models/label.php
@@ -1,3 +1,33 @@
 <?php
 
+class Label extends Table {
+
+
+	// Initialise variables
+	public static int $id;
+	public static string $publicID;
+
+	public static string $name;
+	public static bool $isPublic;
+
+
+
+
+
+	/**
+	 * Label constructor
+	 * 
+	 * @param		string		Label name
+	 * @param		bool		Whether the label is public or not
+	 */
+	public function __construct(string $name, bool $isPublic) {
+
+		$this->name = $name;
+		$this->isPublic = $isPublic;
+
+	}
+
+
+}
+
 ?>
