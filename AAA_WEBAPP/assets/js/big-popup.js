66c54afceb88091f085850fc733e82254a4e38fd
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-30 13:34:17 +0200

    Created class

---
+++ AAA_WEBAPP/assets/js/big-popup.js
@@ -0,0 +1,20 @@
+class BigPopup {
+
+
+	constructor(title, elements) {
+		this.title = title;
+		this.elements = elements;
+	}
+
+
+	show() {
+		
+		const popup = document.getElementById("popup-big");
+
+	}
+
+
+}
+
+const bp = new BigPopup("abc", []);
+bp.show();
