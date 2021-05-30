454c08bc96c9a087e4f63386c9d8d01c79f60bdd
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-30 13:39:13 +0200

    added createElement() method, added javadoc

--- AAA_WEBAPP/assets/js/big-popup.js
+++ AAA_WEBAPP/assets/js/big-popup.js
@@ -1,15 +1,32 @@
 class BigPopup {
 
 
+	/**
+	 * Constructor
+	 * 
+	 * @param {string} title The title of the popup
+	 * @param {Array<HTMLElement>} elements The elements to add
+	 */
 	constructor(title, elements) {
 		this.title = title;
 		this.elements = elements;
 	}
 
 
+	createElement(el, options) {
+
+		const elem = document.createElement(el);
+		for(const [key, value] of Object.entries(options)) {
+			elem.key = value;
+		}
+
+	}
+
+
 	show() {
 		
 		const popup = document.getElementById("popup-big");
+		this.createElement("h2", { innerHTML: "xyz", id: "69" });
 
 	}
 
