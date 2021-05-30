a75efd5a99122d5eebb05cbf8e8f6aaa3904dcf0
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-30 14:59:45 +0200

    Removed small class from input

--- AAA_WEBAPP/dashboard/labels/index.php
+++ AAA_WEBAPP/dashboard/labels/index.php
@@ -86,7 +86,7 @@ session_start();
 		document.getElementById("add-label").addEventListener("click", () => {
 
 			const addLabel = new BigPopup("Add Label", "api/v1/labels/add", "POST");
-			addLabel.add("input", "LabelName", { classList: "small", placeholder: "LABEL NAME" });
+			addLabel.add("input", "LabelName", { placeholder: "LABEL NAME" });
 			addLabel.show("ADD");
 
 		});
