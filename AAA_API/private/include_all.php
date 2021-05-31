70e19de342e1b9aecdd19cb5601719ce3a3e42c4
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-05-31 19:29:28 +0200

    Fixed check

--- AAA_API/private/include_all.php
+++ AAA_API/private/include_all.php
@@ -43,7 +43,7 @@ if(isset($REQUIRE_TOKEN)) {
 	JSONWebToken::validateToken($headerJWT);
 	$payload = JSONWebToken::getPayload($headerJWT);
 
-	if($payload === NULL || $payload->user === NULL) {
+	if($payload === NULL || !isset($payload->user)) {
 		ApiResponse::httpResponse(500, ["error" => "Could not validate your account."]);
 	}
 
