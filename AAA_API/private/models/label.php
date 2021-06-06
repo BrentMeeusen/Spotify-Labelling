961d53c88e2359c5bd72a3db0999aa2cc1ab7f7b
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-06 14:01:22 +0200

    Added findByName() method

--- AAA_API/private/models/label.php
+++ AAA_API/private/models/label.php
@@ -28,6 +28,39 @@ class Label extends Table {
 	}
 
 
+
+
+
+
+
+
+
+
+	/**
+	 * Finds the label by name
+	 * 
+	 * @param		string		The name of the label
+	 * @return		Label		If it was found
+	 * @return		null		If no label was found
+	 */
+	public static function findByName(string $name) : ?Label {
+
+		$stmt = self::prepare("SELECT * FROM LABELS WHERE Name = ?;");
+		$name = self::sanitizeArray([$name])[0];
+		$stmt->bind_param("s", $name);
+		$res = self::getResults($stmt);
+
+		// If no user is found, return NULL
+		if(count($res) === 0) {
+			return NULL;
+		}
+
+		// Create and return the found label as an object
+		return Label::construct($res[0]);
+
+	}
+
+
 }
 
 ?>
