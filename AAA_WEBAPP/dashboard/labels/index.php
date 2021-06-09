9d350b8a63d4e8c9858553e5d6c03f503a7dbd62
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-09 15:44:20 +0200

    Updated parameter to send

--- AAA_WEBAPP/dashboard/labels/index.php
+++ AAA_WEBAPP/dashboard/labels/index.php
@@ -94,7 +94,7 @@ session_start();
 		// Load labels
 		window.addEventListener("load", async () => {
 			const res = await Api.sendRequest("api/v1/labels/all/" + Api.TOKEN.getPayload().user.id, "GET");
-			Api.showLabels(res);
+			Api.showLabels(res.data);
 		});
 
 		</script>
