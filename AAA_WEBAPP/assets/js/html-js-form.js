a36ac6e59876657b09a6d9fc1435b71001289fc8
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-10 15:03:02 +0200

    Added check whether there is a callback or not

--- AAA_WEBAPP/assets/js/html-js-form.js
+++ AAA_WEBAPP/assets/js/html-js-form.js
@@ -52,7 +52,7 @@ class HtmlJsForm {
 
 		// Send the request
 		const res = await Api.sendRequest(this.action, this.method, this.getValues());
-		this.callback();
+		(this.callback ? this.callback() : true);
 
 		// Redirect if necessary
 		if(res.code === 200 && this.redirect !== undefined) {
