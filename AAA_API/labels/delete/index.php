b12d0ecd1a78af58191bc267469702ca7448cb64
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-11 17:16:15 +0200

    Added label finding, additional check whether deleter is label creator

--- AAA_API/labels/delete/index.php
+++ AAA_API/labels/delete/index.php
@@ -16,6 +16,15 @@ if(!isset($payload->rights->label->delete) || $payload->rights->label->delete !=
 }
 
 // If the label isn't found, return an error
+$label = Label::findByPublicID($labelID);
+if($label === NULL) {
+	ApiResponse::httpResponse(404, ["error" => "The label you want to delete was not found."]);
+}
+
+// If deleter is not the creator of the label AND the deleter cannot delete any label, return an error
+if($label->creator !== $payload->user->id && TRUE) {
+	ApiResponse::httpResponse(401, ["error" => "You are not allowed to delete this label."]);
+}
 
 // Delete the label
 
