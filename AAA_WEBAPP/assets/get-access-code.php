8875e31941bfadd41172823195ddfb35ae8e8baa
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-14 19:30:34 +0200

    Added exit() after header redirect

--- AAA_WEBAPP/assets/get-access-code.php
+++ AAA_WEBAPP/assets/get-access-code.php
@@ -3,11 +3,13 @@
 // If we get an "Access denied" error, return to the user
 if(isset($_GET["error"])) {
 	header("Location: php/redirect.php?redirect=&code=001&message=You%20must%20accept%20the%20Spotify%20popup%20if%20you%20want%20to%20use%20this%20application.");
+	exit();
 }
 
 // If we don't have an access code, return an error
 if(!isset($_GET["code"])) {
 	header("Location: php/redirect.php?redirect=&code=001&message=Something%20went%20wrong%20whilst%20getting%20an%20authorisation%20code.");
+	exit();
 }
 
 // If we have a code, redirect to the dashboard
