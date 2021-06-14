a88a30d3850b6a432789ca491aee611417e7bb3c
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-14 19:23:14 +0200

    Fixed unknown parameter bug

--- AAA_WEBAPP/assets/get-access-code.php
+++ AAA_WEBAPP/assets/get-access-code.php
@@ -1,7 +1,7 @@
 <?php
 
 // If we get an "Access denied" error, return to the user
-if($_GET["error"]) {
+if(isset($_GET["error"])) {
 	header("Location: php/redirect.php?redirect=&code=001&message=You%20must%20accept%20the%20Spotify%20popup%20if%20you%20want%20to%20use%20this%20application.");
 }
 
