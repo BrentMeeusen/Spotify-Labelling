3adeeefc5e1c55d9fde5271ad1300c829fa265e3
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-14 19:24:16 +0200

    Added additional check whether we actually have the code

--- AAA_WEBAPP/assets/get-access-code.php
+++ AAA_WEBAPP/assets/get-access-code.php
@@ -5,6 +5,11 @@ if(isset($_GET["error"])) {
 	header("Location: php/redirect.php?redirect=&code=001&message=You%20must%20accept%20the%20Spotify%20popup%20if%20you%20want%20to%20use%20this%20application.");
 }
 
+// If we don't have an access code, return an error
+if(!isset($_GET["code"])) {
+	header("Location: php/redirect.php?redirect=&code=001&message=Something%20went%20wrong%20whilst%20getting%20an%20authorisation%20code.");
+}
+
 
 print("<pre>");
 print_r($_GET);
