8f44d9ee07fbaae28ba0d7bfa4e6cf6a1140e5f3
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-14 18:06:13 +0200

    Added comment containing info about created bug

--- AAA_WEBAPP/dashboard/index.php
+++ AAA_WEBAPP/dashboard/index.php
@@ -61,6 +61,8 @@ session_start();
 		// Protect the page
 		PageProtect.protect({ verifiedLevel: 2 });
 
+		// BUG! IF GET->CODE IS SET, DON'T REDIRECT
+
 		// Force login
 		window.location.href = (`https://accounts.spotify.com/authorize?response_type=code
 		&client_id=a209cbda1aaa4f408bd6ae2efc2264fb&redirect_uri=http%3A%2F%2Flocalhost%2FSpotify%20Labelling%2FAAA_WEBAPP%2Fdashboard
