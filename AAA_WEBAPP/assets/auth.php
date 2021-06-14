7ba88613a825a4e4acd21af68e1375f624c0fe9a
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-14 19:10:57 +0200

    Removed authorisation

---
+++ AAA_WEBAPP/assets/auth.php
@@ -0,0 +1,12 @@
+<?php
+header("Location: https://accounts.spotify.com/authorize?response_type=code
+&client_id=a209cbda1aaa4f408bd6ae2efc2264fb&redirect_uri=http%3A%2F%2Flocalhost%2FSpotify%20Labelling%2FAAA_WEBAPP%2Fdashboard
+&scope=user-read-email
+%20user-read-private
+%20playlist-modify-public
+%20playlist-modify-private
+%20playlist-read-private
+%20playlist-read-collaborative
+%20user-library-read
+&show_dialog=true");
+?>
--- AAA_WEBAPP/dashboard/index.php
+++ AAA_WEBAPP/dashboard/index.php
@@ -51,38 +51,25 @@ session_start();
 
 
 
+
+
 		<?php
+
 		// Load JavaScript
 		include_once("../assets/snippets/javascript-files.php");
+
+		// Load showing result after redirect
+		include_once("../assets/snippets/show-redirect-result.php");
+
 		?>
-		
+
+
+
 		<script>
 
 		// Protect the page
 		PageProtect.protect({ verifiedLevel: 2 });
 
-		// BUG! IF GET->CODE IS SET, DON'T REDIRECT
-
-		// Force login
-		window.location.href = (`https://accounts.spotify.com/authorize?response_type=code
-		&client_id=a209cbda1aaa4f408bd6ae2efc2264fb&redirect_uri=http%3A%2F%2Flocalhost%2FSpotify%20Labelling%2FAAA_WEBAPP%2Fdashboard
-		&scope=user-read-email
-		%20user-read-private
-		%20playlist-modify-public
-		%20playlist-modify-private
-		%20playlist-read-private
-		%20playlist-read-collaborative
-		%20user-library-read
-		&show_dialog=true`);
-		</script>
-
-		<?php
-		// Load showing result after redirect
-		include_once("../assets/snippets/show-redirect-result.php");
-		?>
-
-		<script>
-		
 		</script>
 
 	</body>
