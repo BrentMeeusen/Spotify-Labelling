1fb4a4f7893c4743e0d66f700a8a1eb36a49b164
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-15 23:55:53 +0200

    Added constructor

--- AAA_API/private/models/playlist.php
+++ AAA_API/private/models/playlist.php
@@ -9,6 +9,23 @@ class Playlist {
 	private int $numTracks;
 	private string $spotifyID;
 
+
+
+
+
+	/**
+	 * Constructor
+	 * 
+	 * @param		StdClass	The data from Spotify
+	 */
+	public function __construct(StdClass $data) {
+
+		$this->name = $data->name;
+		$this->numTracks = $data->tracks->total;
+		$this->spotifyID = $data->id;
+
+	}
+
 }
 
 
