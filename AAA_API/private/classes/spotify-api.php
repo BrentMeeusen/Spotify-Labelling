ad8e95a801b8300e27261eea4e7c4ef1e9ed9287
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-15 22:57:54 +0200

    Added setAuthorisationToken() method and token varaible

--- AAA_API/private/classes/spotify-api.php
+++ AAA_API/private/classes/spotify-api.php
@@ -2,7 +2,24 @@
 
 
 class SpotifyApi {
-	
+
+
+	// Declare variables
+	private static string $authorisationToken;
+
+
+
+
+
+	/**
+	 * Sets the authorisation token
+	 * 
+	 * @param		string		The token
+	 */
+	public static string setAuthorisationToken(string $token) : void {
+		self::$authorisationToken = $token;
+	}
+
 }
 
 
