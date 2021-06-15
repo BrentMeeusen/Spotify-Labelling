e364f18d652a761cd9cbcff156bf8344c301bf5f
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-15 23:47:48 +0200

    Added create() class

--- AAA_API/private/models/playlists.php
+++ AAA_API/private/models/playlists.php
@@ -2,7 +2,25 @@
 
 
 class Playlists {
-	
+
+
+	// Declare variables
+	private array $playlists;
+
+
+
+
+
+	/**
+	 * Formats the found playlists in this collection
+	 * 
+	 * @param		array		A playlist array that Spotify sent
+	 * @return		Playlists	A playlists object with the given playlists
+	 */
+	public static function create(array $playlists) : Playlists {
+
+	}
+
 }
 
 ?>
