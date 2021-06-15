b9c45f871fd9b34b80dfa41498d7d73e1469575b
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-15 18:59:59 +0200

    Updated show_dialog

--- AAA_WEBAPP/assets/auth.php
+++ AAA_WEBAPP/assets/auth.php
@@ -1,4 +1,4 @@
 <?php
 // WARNING: HARDCODED
-header("Location: https://accounts.spotify.com/authorize?response_type=code&client_id=a209cbda1aaa4f408bd6ae2efc2264fb&redirect_uri=http%3A%2F%2Flocalhost%2FSpotify%20Labelling%2FAAA_WEBAPP%2Fassets%2Fget-access-code.php&scope=user-read-email%20user-read-private%20playlist-modify-public%20playlist-modify-private%20playlist-read-private%20playlist-read-collaborative%20user-library-read&show_dialog=true");
+header("Location: https://accounts.spotify.com/authorize?response_type=code&client_id=a209cbda1aaa4f408bd6ae2efc2264fb&redirect_uri=http%3A%2F%2Flocalhost%2FSpotify%20Labelling%2FAAA_WEBAPP%2Fassets%2Fget-access-code.php&scope=user-read-email%20user-read-private%20playlist-modify-public%20playlist-modify-private%20playlist-read-private%20playlist-read-collaborative%20user-library-read&show_dialog=false");
 ?>
