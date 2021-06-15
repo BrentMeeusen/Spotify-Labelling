a48d73c342914417370e5d980881db0317c6a564
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-15 22:52:49 +0200

    Created file with comments on what should be done

---
+++ AAA_API/spotify/playlists/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$ALLOWED_METHOD = "GET";
+$REQUIRE_TOKEN = TRUE;
+
+include_once("../../private/include_all.php");
+
+
+
+// Get the JWT
+// Get the authorisation token from the JWT
+// Send a request to the endpoint at Spotify
+// Parse the data using the models
+// Return the useful playlist data
+
+
+
+?>
