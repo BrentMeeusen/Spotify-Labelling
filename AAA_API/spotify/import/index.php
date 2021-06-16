5e5a9fa1a6f76a2344f4c94b16a49e46da598b00
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-16 15:59:35 +0200

    Wrote code for importing songs from Spotify

---
+++ AAA_API/spotify/import/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$ALLOWED_METHOD = "POST";
+$REQUIRE_TOKEN = TRUE;
+
+include_once("../../private/include_all.php");
+
+
+
+// Get the authorisation token from the JWT
+SpotifyApi::setAuthorisationToken($payload->user->accessToken);
+
+// Send a request to the endpoint at Spotify
+$data = SpotifyApi::getSongsFromPlaylist($_GET["id"]);
+
+// Parse the data using the models
+$songs = Songs::create($data);
+
+// Store them in the database
+Spotify::store($songs);
+
+// Return the useful playlist data
+ApiResponse::httpResponse(200, ["message" => "Imported your songs successfully."]);
+
+
+?>
