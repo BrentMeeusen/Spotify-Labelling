5482183a9c57cad73defa09e421a36b4edf0c0a6
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-16 16:22:42 +0200

    Added constructor

--- AAA_API/private/models/tracks.php
+++ AAA_API/private/models/tracks.php
@@ -3,6 +3,24 @@
 
 class Tracks {
 
+
+	// Declare variables
+	public array $tracks;
+
+
+
+
+
+	/**
+	 * Constructor
+	 * 
+	 * @param		array		An array of Track objects
+	 */
+	public function __construct(array $tracks) {
+		$this->tracks = $tracks;
+	}
+
+
 }
 
 ?>
