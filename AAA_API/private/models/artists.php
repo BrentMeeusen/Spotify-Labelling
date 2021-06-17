1daf70a4d2df4b5069c2d4223792d65042307f56
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-17 15:20:51 +0200

    Added varaibles and constructor

--- AAA_API/private/models/artists.php
+++ AAA_API/private/models/artists.php
@@ -3,6 +3,23 @@
 
 class Artists {
 
+
+	// Declare variables
+	public array $artists;
+
+
+
+
+
+	/**
+	 * Constructor
+	 * 
+	 * @param		array		An array of Artist objects
+	 */
+	public function __construct(array $artists) {
+		$this->artists = $artists;
+	}
+
 }
 
 ?>
