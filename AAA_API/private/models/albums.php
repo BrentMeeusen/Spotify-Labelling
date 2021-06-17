66e71ae0466fb34a988dc945dbfa13901b37e3e4
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-17 15:30:44 +0200

    Added code to implement the interface

--- AAA_API/private/models/albums.php
+++ AAA_API/private/models/albums.php
@@ -2,6 +2,43 @@
 
 
 class Albums implements SpotifyCollection {
+
+
+	// Declare variables
+	public array $albums;
+
+
+
+
+
+	/**
+	 * Constructor
+	 * 
+	 * @param		array		An array of Album objects
+	 */
+	public function __construct(array $albums) {
+		$this->albums = $albums;
+	}
+
+
+
+
+
+	/**
+	 * Formats the given albums in a collection
+	 * 
+	 * @param		array		An album array that Spotify sent
+	 * @return		Albums		An Albums object with the given playlists
+	 */
+	public static function create(array $albums) : Albums {
+
+		$newAlbums = [];
+		foreach($albums as $album) {
+			array_push($newAlbums, new Album($album));
+		}
+		return new Albums($newAlbums);
+
+	}
 	
 }
 
