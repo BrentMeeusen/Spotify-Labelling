fcf6d3f13faa496f2c4c992546a2aaaa046b69b3
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-17 15:39:55 +0200

    Added javadoc to interface

--- AAA_API/private/models/AAA_spotify.php
+++ AAA_API/private/models/AAA_spotify.php
@@ -16,8 +16,28 @@ class Spotify {
 
 interface SpotifyCollection {
 
+	
+	/**
+	 * Gets the data from the collection
+	 * 
+	 * @return		array		The data
+	 */
 	public function getData() : array;
 
+
+	/**
+	 * Stores the entire collection in the database
+	 * 
+	 * @return		bool		Whether it was a success or not
+	 */
+	public function store() : bool;
+
+
+	/**
+	 * Creates a collection object from the given data
+	 * 
+	 * @param		array		The objects to create from Spotify's data
+	 */
 	public static function create(array $data) : SpotifyCollection;
 
 }
