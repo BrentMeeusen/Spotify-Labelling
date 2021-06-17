cee1c92ed10c5ada74985df7cf06d53a8e83308b
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-17 15:23:28 +0200

    Added variables and constructor

--- AAA_API/private/models/artist.php
+++ AAA_API/private/models/artist.php
@@ -3,6 +3,27 @@
 
 class Artist {
 
+
+	// Declare variables
+	public string $name;
+	public string $spotifyID;
+
+
+
+
+
+	/**
+	 * Constructor
+	 * 
+	 * @param		StdClass	The data from Spotify
+	 */
+	public function __construct(StdClass $data) {
+
+		$this->name = $data->name;
+		$this->spotifyID = $data->id;
+
+	}
+
 }
 
 ?>
