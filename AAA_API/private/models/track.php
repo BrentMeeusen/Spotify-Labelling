e6c82367bc44326765d13c35fac74ebfeb8ec8e2
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-17 15:09:33 +0200

    Updated artists variable

--- AAA_API/private/models/track.php
+++ AAA_API/private/models/track.php
@@ -6,7 +6,7 @@ class Track {
 
 	// Declare variables
 	public Album $album;
-	public array $artists;
+	public Artists $artists;
 	public string $name;
 	public string $spotifyID;
 
@@ -22,7 +22,7 @@ class Track {
 	public function __construct(StdClass $data) {
 
 		$this->album = new Album($data->track->album);
-		$this->artists = new Artists($data->track->artists);
+		$this->artists = new Artists::create($data->track->artists);
 		$this->name = $data->track->name;
 		$this->spotifyID = $data->track->id;
 
