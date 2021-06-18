2d13deae9831ff79563ea96505da4d4d1bdbc16a
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-19 01:11:25 +0200

    Updated redirects

--- AAA_WEBAPP/assets/php/get-access-code.php
+++ AAA_WEBAPP/assets/php/get-access-code.php
@@ -5,13 +5,13 @@ $VARIABLES = json_decode(file_get_contents("../../../SpotifyLabelling.json"));
 
 // If we get an "Access denied" error, return to the user
 if(isset($_GET["error"])) {
-	header("Location: php/redirect.php?redirect=&code=001&message=You%20must%20accept%20the%20Spotify%20popup%20if%20you%20want%20to%20use%20this%20application.");
+	header("Location: redirect.php?redirect=&code=001&message=You%20must%20accept%20the%20Spotify%20popup%20if%20you%20want%20to%20use%20this%20application.");
 	exit();
 }
 
 // If we don't have an access code, return an error
 if(!isset($_GET["code"])) {
-	header("Location: php/redirect.php?redirect=&code=001&message=Something%20went%20wrong%20whilst%20getting%20an%20authorisation%20code.");
+	header("Location: redirect.php?redirect=&code=001&message=Something%20went%20wrong%20whilst%20getting%20an%20authorisation%20code.");
 	exit();
 }
 
@@ -38,7 +38,7 @@ $token = @json_decode($res)->access_token;
 
 // If there is no token, throw an error
 if($token === NULL) {
-	header("Location: php/redirect.php?redirect=&code=001&message=Something%20went%20wrong%20whilst%20getting%20an%20authorisation%20code.");
+	header("Location: redirect.php?redirect=&code=001&message=Something%20went%20wrong%20whilst%20getting%20an%20authorisation%20code.");
 	exit();
 }
 
@@ -62,7 +62,7 @@ $jwt = json_decode($res)->jwt;
 setcookie("jwt", $jwt, time() + 3600, "/");
 
 // Redirect to the dashboard
-header("Location: php/redirect.php?redirect=dashboard&code=200&message=Logged%20in%20successfully.");
+header("Location: redirect.php?redirect=dashboard&code=200&message=Logged%20in%20successfully.");
 exit();
 
 ?>
