ddd274953ba5fdf266de895328050aa22720741f
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-21 18:00:09 +0200

    Added code for getting the track

---
+++ AAA_API/tracks/get/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$ALLOWED_METHOD = "GET";
+$REQUIRE_TOKEN = TRUE;
+
+include_once("../../private/include_all.php");
+
+
+
+// Get all tracks from this user
+Database::getTracksFromUser($payload->user->id);
+
+// Properly return the results
+ApiResponse::httpResponse(200, [ "message" => "Tracks found.", "data" => $res ]);
+
+?>
