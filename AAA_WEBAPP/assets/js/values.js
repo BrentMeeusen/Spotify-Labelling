59e05daf304203a6cc2167978cb7e52a0590d61e
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-21 17:12:32 +0200

    Updated hardcoded values

--- AAA_WEBAPP/assets/js/values.js
+++ AAA_WEBAPP/assets/js/values.js
@@ -1,9 +1,5 @@
-// WARNING: HARDCODED
 const VALUES = {
-	assets: "/Spotify Labelling/AAA_WEBAPP/assets/",
-	assetsOnline: "/assets/",
-
-	api: "http://localhost/Spotify Labelling/AAA_API/",
-	apiOnline: "http://spotify-labelling-api.21webb.nl/",
+	assets: "/assets/",
+	api: "http://spotify-labelling-api.21webb.nl/",
 
 };
--- AAA_WEBAPP/assets/snippets/css.php
+++ AAA_WEBAPP/assets/snippets/css.php
@@ -1,8 +1,7 @@
-<!-- WARNING: HARDCODED -->
-<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/general.css">
-<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/dashboard.css">
-<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/form.css">
-<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/lazy-loading.css">
-<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/module.css">
-<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/navigation.css">
-<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/popup.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/general.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/dashboard.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/form.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/lazy-loading.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/module.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/navigation.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/popup.css">
