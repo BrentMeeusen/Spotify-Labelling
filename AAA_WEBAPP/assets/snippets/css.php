5be86c84e23078c9e3cefd3e28cc3eff2fdeee06
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-21 16:57:32 +0200

    Added CSS links

---
+++ AAA_WEBAPP/assets/snippets/css.php
@@ -0,0 +1,8 @@
+<!-- WARNING: HARDCODED -->
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/dashboard.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/form.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/general.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/lazy-loading.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/module.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/navigation.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/popup.css">
