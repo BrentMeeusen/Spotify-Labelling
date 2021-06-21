cf7610adc89c87cd33050ef49b40fa1361a5d023
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-21 16:30:55 +0200

    Add hardcode warning

--- AAA_WEBAPP/assets/php/auth.php
+++ AAA_WEBAPP/assets/php/auth.php
@@ -1,3 +1,4 @@
 <?php
+// WARNING: HARDCODED; SET redirect_uri=http%3A%2F%2Fwww.spotify-labelling%2Fassets%2Fphp%2Fget-access-code.php
 header("Location: https://accounts.spotify.com/authorize?response_type=code&client_id=a209cbda1aaa4f408bd6ae2efc2264fb&redirect_uri=http%3A%2F%2Flocalhost%2FSpotify%20Labelling%2FAAA_WEBAPP%2Fassets%2Fphp%2Fget-access-code.php&scope=user-read-email%20user-read-private%20playlist-modify-public%20playlist-modify-private%20playlist-read-private%20playlist-read-collaborative%20user-library-read&show_dialog=false");
 ?>
