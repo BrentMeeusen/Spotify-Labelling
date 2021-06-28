a49bd58d11db79d087b07a42bc5b7a71d110a8cd
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-28 19:26:38 +0200

    Created constructor

--- AAA_API/private/models/spotify/spotify-artist.php
+++ AAA_API/private/models/spotify/spotify-artist.php
@@ -2,6 +2,27 @@
 
 class SpotifyArtist implements SpotifyData {
 
+
+	// Declare variables
+	public string $name;
+	public string $id;
+
+
+
+
+
+	/**
+	 * Constructor
+	 * 
+	 * @param		StdClass	The album data
+	 */
+	public function __construct(StdClass $data) {
+
+		$this->name = $data->name;
+		$this->id = $data->id;
+
+	}
+
 }
 
 ?>
