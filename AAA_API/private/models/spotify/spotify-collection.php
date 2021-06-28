a3afb260653f0d90296920a32d42e82ee4c974b4
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-28 17:44:14 +0200

    Added createPlaylists() method

--- AAA_API/private/models/spotify/spotify-collection.php
+++ AAA_API/private/models/spotify/spotify-collection.php
@@ -2,10 +2,36 @@
 
 class SpotifyCollection {
 
+
+	/**
+	 * Creates a collection of playlists
+	 * 
+	 * @param		array				The playlists
+	 * @return		SpotifyCollection	The collection
+	 */
+	public static function createPlaylists(array $playlists) : SpotifyCollection {
+
+		$lists = [];
+		foreach($playlists as $list) {
+			array_push($lists, new Playlist($list));
+		}
+		return new SpotifyCollection($lists);
+
+	}
+
 }
 
+
+
+
+
+
+
+
+
+
 interface SpotifyData {
-	
+
 }
 
 ?>
