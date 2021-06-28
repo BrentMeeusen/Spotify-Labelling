f96936e3149f9f9d38b208f70a7e024292d6dab1
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-28 19:22:17 +0200

    Created constructor

--- AAA_API/private/models/spotify/spotify-track.php
+++ AAA_API/private/models/spotify/spotify-track.php
@@ -2,6 +2,33 @@
 
 class SpotifyTrack implements SpotifyData {
 
+
+	// Declare variables
+	public SpotifyAlbum $album;
+	public SpotifyCollection $artists;
+	public string $name;
+	public string $releaseDate;
+	public string $id;
+
+
+
+
+
+	/**
+	 * Constructor
+	 * 
+	 * @param		StdClass	The data from Spotify
+	 */
+	public function __construct(StdClass $data) {
+
+		$this->album = new SpotifyAlbum($data->track->album);
+		$this->artists = SpotifyCollection::createArtistCollection($data->track->artists);
+		$this->name = $data->track->name;
+		$this->releaseDate = $data->track->album->release_date;
+		$this->id = $data->track->id;
+
+	}
+
 }
 
 ?>
