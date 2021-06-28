a5e698dec6c062f531c12da8f90e2cde4cc9304d
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-28 17:35:01 +0200

    Created SpotifyPlaylist constructor

--- AAA_API/private/models/spotify/spotify-playlist.php
+++ AAA_API/private/models/spotify/spotify-playlist.php
@@ -1,7 +1,30 @@
 <?php
 
 class SpotifyPlaylist extends SpotifyData {
-	
+
+
+	// Declare variables
+	public string $name;
+	public int $numTracks;
+	public string $id;
+
+
+
+
+
+	/**
+	 * Creates a playlist based on a JSON decoded Spotify response
+	 * 
+	 * @param		StdClass	The playlist data
+	 */
+	public function __construct(StdClass $data) {
+
+		$this->name = $data->name;
+		$this->numTracks = $data->tracks->total;
+		$this->spotifyID = $data->id;
+
+	}
+
 }
 
 ?>
