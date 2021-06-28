2ece7ce94bc2f0dcf66436a2c08f52befbf435d5
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-28 16:02:09 +0200

    Updated hardcoded values

--- AAA_WEBAPP/assets/snippets/javascript-files.php
+++ AAA_WEBAPP/assets/snippets/javascript-files.php
@@ -1,18 +1,19 @@
 <!-- Load all files -->
-<script src="/assets/js/api.js"></script>
-<script src="/assets/js/html-js-form.js"></script>
-<script src="/assets/js/big-popup.js"></script>
-<script src="/assets/js/jwt.js"></script>
-<script src="/assets/js/lazy-loading.js"></script>
-<script src="/assets/js/navigation.js"></script>
-<script src="/assets/js/page-protect.js"></script>
-<script src="/assets/js/password-verifier.js"></script>
-<script src="/assets/js/popup.js"></script>
-<script src="/assets/js/theme.js"></script>
-<script src="/assets/js/values.js"></script>
+<!-- WARNING: HARDCODED -->
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/api.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/html-js-form.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/big-popup.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/jwt.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/lazy-loading.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/navigation.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/page-protect.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/password-verifier.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/popup.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/theme.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/values.js"></script>
 
 <!-- Load general file (which will be a minified version of all the files above that are general) -->
-<script src="/assets/js/general.js"></script>
+<script src="/Spotify Labelling/AAA_WEBAPP/assets/js/general.js"></script>
 
 <!-- Set JWT -->
 <?php if(isset($_COOKIE["jwt"])) { print('<script>Api.TOKEN = new JWT("' . $_COOKIE["jwt"] . '"); </script>'); } ?>
