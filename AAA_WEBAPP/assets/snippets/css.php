0e06d8f1ead3e800efd88a69b5edee0a60165a0d
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-28 16:01:32 +0200

    Updated hardcoded values

--- AAA_WEBAPP/assets/snippets/css.php
+++ AAA_WEBAPP/assets/snippets/css.php
@@ -1,7 +1,8 @@
-<link rel="stylesheet" type="text/css" href="/assets/css/general.css">
-<link rel="stylesheet" type="text/css" href="/assets/css/dashboard.css">
-<link rel="stylesheet" type="text/css" href="/assets/css/form.css">
-<link rel="stylesheet" type="text/css" href="/assets/css/lazy-loading.css">
-<link rel="stylesheet" type="text/css" href="/assets/css/module.css">
-<link rel="stylesheet" type="text/css" href="/assets/css/navigation.css">
-<link rel="stylesheet" type="text/css" href="/assets/css/popup.css">
+<!-- WARNING: HARDCODED -->
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/general.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/dashboard.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/form.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/lazy-loading.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/module.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/navigation.css">
+<link rel="stylesheet" type="text/css" href="/Spotify Labelling/AAA_WEBAPP/assets/css/popup.css">
