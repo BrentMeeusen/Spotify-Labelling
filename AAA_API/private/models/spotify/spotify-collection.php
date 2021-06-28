b73c01fb99bbcbf61ad14ae9036506e31d56d441
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-28 17:46:04 +0200

    Created SpotifyCollection constructor

--- AAA_API/private/models/spotify/spotify-collection.php
+++ AAA_API/private/models/spotify/spotify-collection.php
@@ -3,7 +3,27 @@
 class SpotifyCollection {
 
 
+	// Declare variables
+	public array $data;
+
+
+
+
+
 	/**
+	 * Creates a collection
+	 * 
+	 * @param		array		The data to create a collection from
+	 */
+	public function __construct(array $data) {
+		$this->data = $data;
+	}
+
+
+
+
+
+	 /**
 	 * Creates a collection of playlists
 	 * 
 	 * @param		array				The playlists
