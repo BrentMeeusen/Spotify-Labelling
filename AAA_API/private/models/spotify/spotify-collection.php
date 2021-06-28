441da5e16171748e827593ba902d4e5f3961f66c
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-28 19:27:41 +0200

    Created createArtistCollection() method

--- AAA_API/private/models/spotify/spotify-collection.php
+++ AAA_API/private/models/spotify/spotify-collection.php
@@ -23,6 +23,11 @@ class SpotifyCollection {
 
 
 
+
+
+
+
+
 	/**
 	 * Creates a collection of tracks
 	 * 
@@ -43,19 +48,39 @@ class SpotifyCollection {
 
 
 
+	/**
+	 * Creates a collection of artists
+	 * 
+	 * @param		array				The artists
+	 * @return		SpotifyCollection	The collection
+	 */
+	public static function createArtistCollection(array $data) : SpotifyCollection {
+
+		$tracks = [];
+		foreach($data as $track) {
+			array_push($tracks, new SpotifyTrack($track));
+		}
+		return new SpotifyCollection($tracks);
+
+	}
+
+
+
+
+
 	 /**
 	 * Creates a collection of playlists
 	 * 
 	 * @param		array				The playlists
 	 * @return		SpotifyCollection	The collection
 	 */
-	public static function createPlaylistCollection(array $playlists) : SpotifyCollection {
+	public static function createPlaylistCollection(array $data) : SpotifyCollection {
 
-		$lists = [];
-		foreach($playlists as $list) {
-			array_push($lists, new SpotifyPlaylist($list));
+		$playlists = [];
+		foreach($data as $list) {
+			array_push($playlists, new SpotifyPlaylist($list));
 		}
-		return new SpotifyCollection($lists);
+		return new SpotifyCollection($playlists);
 
 	}
 
