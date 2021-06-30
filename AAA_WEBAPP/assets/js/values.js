7c8b1b836172ab0a0aa9035529afd69e27f8c5ed
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-30 14:07:39 +0200

    Updated hardcoded values

--- AAA_WEBAPP/assets/js/values.js
+++ AAA_WEBAPP/assets/js/values.js
@@ -1,8 +1,4 @@
-// WARNING: HARDCODED
 const VALUES = {
-	assets: "/Spotify Labelling/AAA_WEBAPP/assets/",
-	assetsOnline: "/assets/",
-
-	api: "/Spotify Labelling/AAA_API/",
-	apiOnline: "http://spotify-labelling-api.21webb.nl/"
+	assets: "/assets/",
+	api: "http://spotify-labelling-api.21webb.nl/"
 };
--- AAA_WEBAPP/assets/snippets/navigation.php
+++ AAA_WEBAPP/assets/snippets/navigation.php
@@ -1,23 +1,22 @@
 <!-- Menu -->
-<!-- WARNING: HARDCODED -->
 <div class="nav-btn" id="nav-open">
-	<img src="/Spotify Labelling/AAA_WEBAPP/assets/icons/menu/menu-open.png">
+	<img src="/assets/icons/menu/menu-open.png">
 </div>
 			
 <div class="nav" id="nav-menu">
 	<div class="nav-btn" id="nav-close">
-		<img src="/Spotify Labelling/AAA_WEBAPP/assets/icons/menu/menu-close.png">
+		<img src="/assets/icons/menu/menu-close.png">
 	</div>
 	
 	<ul>
-		<li><a href="/Spotify Labelling/AAA_WEBAPP/dashboard/">Dashboard</a></li>
-		<li><a href="/Spotify Labelling/AAA_WEBAPP/how-it-works/">How It Works</a></li>
-		<li><a href="/Spotify Labelling/AAA_WEBAPP/dashboard/import/">Import Songs</a></li>
-		<li><a href="/Spotify Labelling/AAA_WEBAPP/dashboard/songs/">Your Songs</a></li>
-		<li><a href="/Spotify Labelling/AAA_WEBAPP/dashboard/labels/">Your Labels</a></li>
-		<li><a href="/Spotify Labelling/AAA_WEBAPP/dashboard/playlists/">Your Playlists</a></li>
-		<li><a href="/Spotify Labelling/AAA_WEBAPP/dashboard/account/">Your Account</a></li>
-		<li><a href="/Spotify Labelling/AAA_WEBAPP/logout/">Logout</a></li>
+		<li><a href="/dashboard/">Dashboard</a></li>
+		<li><a href="/how-it-works/">How It Works</a></li>
+		<li><a href="/dashboard/import/">Import Songs</a></li>
+		<li><a href="/dashboard/songs/">Your Songs</a></li>
+		<li><a href="/dashboard/labels/">Your Labels</a></li>
+		<li><a href="/dashboard/playlists/">Your Playlists</a></li>
+		<li><a href="/dashboard/account/">Your Account</a></li>
+		<li><a href="/logout/">Logout</a></li>
 	</ul>
 </div>
 
