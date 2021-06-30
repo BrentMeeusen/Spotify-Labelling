b201cccbfbc01aaef1b402830b5cf3a57eb246eb
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-30 11:40:06 +0200

    Included all old files again

--- AAA_API/private/include_all.php
+++ AAA_API/private/include_all.php
@@ -19,17 +19,17 @@ include_once("models/spotify/spotify-artist.php");
 include_once("models/spotify/spotify-playlist.php");
 include_once("models/spotify/spotify-track.php");
 
-// include_once("models/AAA_spotify.php");
-// include_once("models/album.php");
-// include_once("models/albums.php");
-// include_once("models/artist.php");
-// include_once("models/artists.php");
-// include_once("models/label.php");
-// include_once("models/playlist.php");
-// include_once("models/playlists.php");
-// include_once("models/track.php");
-// include_once("models/tracks.php");
+include_once("models/AAA_spotify.php");
 include_once("models/AAA_table.php");
+include_once("models/album.php");
+include_once("models/albums.php");
+include_once("models/artist.php");
+include_once("models/artists.php");
+include_once("models/label.php");
+include_once("models/playlist.php");
+include_once("models/playlists.php");
+include_once("models/track.php");
+include_once("models/tracks.php");
 include_once("models/user.php");
 
 include_once("methods.php");
