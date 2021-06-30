5daef9394ff2c999a5c9884bbb73828df32f7871
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-06-30 12:13:04 +0200

    Removed commented code

--- AAA_API/spotify/import/index.php
+++ AAA_API/spotify/import/index.php
@@ -21,30 +21,12 @@ $result = $tracks->store($payload->user->id);
 
 
 
-// print(json_encode([$tracks, $result], JSON_PRETTY_PRINT));
-// exit();
-
-
-
-// // Get the authorisation token from the JWT
-// SpotifyApi::setAuthorisationToken($payload->user->accessToken);
-
-// // Send a request to the endpoint at Spotify
-// $data = SpotifyApi::getTracksFromPlaylist($_GET["id"]);
-
-// // Parse the data using the models
-// $tracks = Tracks::create($data);
-
-// // Store them in the database
-// $res = $tracks->store($payload);
-
 // If something went wrong, return an error
-if($res === FALSE) {
+if($result === FALSE) {
 	ApiResponse::httpResponse(500, ["error" => "Something went wrong whilst importing your songs."]);
 }
 
 // Return success
 ApiResponse::httpResponse(200, ["message" => "Imported your songs successfully."]);
-
 
 ?>
