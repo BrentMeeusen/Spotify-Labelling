14082b0d6dbc2be6b4beedadd5119fce1f038750
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-07-06 12:36:10 +0200

    Updated logics

--- AAA_API/tracks/get/index.php
+++ AAA_API/tracks/get/index.php
@@ -7,8 +7,16 @@ include_once("../../private/include_all.php");
 
 
 
-// Get all tracks from this user
+// Get all tracks from this user in a collection
 $res = Database::findTracksByUser($payload->user->id);
+
+// Merge all tracks if the IDs are the same
+$res = $res->merge();
+
+// If the merging went wrong, return an error
+if($res === NULL) {
+	ApiResponse::httpResponse(500, ["error" => "Something went wrong with getting your tracks."]);
+}
 
 // Properly return the results
 ApiResponse::httpResponse(200, [ "message" => "Tracks found.", "data" => $res ]);
