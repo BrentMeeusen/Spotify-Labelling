4e8543a8f64061426d884db349e48cbe8817bdeb
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-07-06 12:46:42 +0200

    Created IAlbum class

--- AAA_API/private/models/my/album.php
+++ AAA_API/private/models/my/album.php
@@ -2,6 +2,43 @@
 
 class IAlbum {
 
+
+	// Declare variables
+	public string $id;
+	public string $name;
+
+
+
+
+
+	/**
+	 * Creates an album
+	 * 
+	 * @param		StdClass	The data
+	 */
+	public function __construct(StdClass $data) {
+		$this->id = $data->id;
+		$this->name = $data->name;
+	}
+
+
+
+
+
+	/**
+	 * Creates an album from the data that the track returned
+	 * 
+	 * @param		StdClass	The track data
+	 * @return		IAlbum		The created album
+	 */
+	public static function createFromTrack(StdClass $trackData) : IAlbum {
+
+		@$albumData->id = $trackData->AlbumID;
+		$albumData->name = $trackData->AlbumName;
+		return new IAlbum($albumData);
+
+	}
+
 }
 
 ?>
