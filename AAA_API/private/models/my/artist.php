bac9b2ec445c8a2aabcf62d27e1dce3b74e80588
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-07-09 23:17:27 +0200

    Added equals(), findBySpotifyId(), createFromDatabase() methods

--- AAA_API/private/models/my/artist.php
+++ AAA_API/private/models/my/artist.php
@@ -39,6 +39,69 @@ class IArtist {
 
 	}
 
+
+
+
+
+	/**
+	 * Creates an artist from the data that the database returned
+	 * 
+	 * @param		StdClass	The artist data
+	 * @return		IArtist		The created artist
+	 */
+	public static function createFromDatabase(StdClass $artist) : IArtist {
+
+		@$data->id = $artist->SpotifyID;
+		$data->name = $artist->Name;
+		return new IArtist($data);
+
+	}
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * Finds an artist from our database given a Spotify ID
+	 * 
+	 * @param		string		The Spotify ID to search for
+	 * @return		IArtist		If it was found
+	 * @return		null		If it was not found
+	 */
+	public static function findBySpotifyId(string $id) : ?IArtist {
+
+		$data = Database::find("SELECT * FROM ARTISTS WHERE SpotifyID = ?;", $id);
+		return (isset($data[0]) ? self::createFromDatabase($data[0]) : NULL);
+
+	}
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * Checks whether everything is equal
+	 * 
+	 * @param		IArtist		The artist to compare to
+	 * @return		bool		Whether they're equal or not
+	 */
+	public function equals(IArtist $artist) : bool {
+
+		return $this->id === $artist->id &&
+				$this->name === $artist->name;
+
+	}
+
 }
 
 ?>
