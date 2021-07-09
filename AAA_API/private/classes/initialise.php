5890a3e9593937fe81ef76558c31f84b9cbeb8e7
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-07-09 23:51:09 +0200

    Removed first name, last name, and username

--- AAA_API/private/classes/initialise.php
+++ AAA_API/private/classes/initialise.php
@@ -43,9 +43,6 @@ class Initialise extends Database {
 		$SQL = "CREATE TABLE $tableName (
 			ID				INT(11)			NOT NULL 	AUTO_INCREMENT,
 			PublicID		VARCHAR(32)		NOT NULL,
-			FirstName		VARCHAR(50) 	NOT NULL,
-			LastName		VARCHAR(50)		NOT NULL,
-			Username		VARCHAR(100)	NOT NULL,
 			EmailAddress	VARCHAR(250)	NOT NULL,
 			Password		VARCHAR(256)	NOT NULL,
 			AccountStatus	INT(1)			NOT NULL,
