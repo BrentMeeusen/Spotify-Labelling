09577453aa1835e0079fcb617abf5dcdcb7ab0a8
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-14 15:44:15 +0200

    Made track update on removing song from application

--- AAA_WEBAPP/assets/js/option-popup.js
+++ AAA_WEBAPP/assets/js/option-popup.js
@@ -21,8 +21,10 @@ class OptionPopup {
 
 		// Add "delete track" row
 		const deleteTrack = Api.createElement("div", { classList: "row" });
-		deleteTrack.addEventListener("click", () => {
-			Api.request("api/v1/tracks/" + track.id + "/delete", "DELETE");
+		deleteTrack.addEventListener("click", async () => {
+			await Api.request("api/v1/tracks/" + track.id + "/delete", "DELETE");
+			const tracks = await Api.sendRequest("api/v1/tracks/get", "GET");
+			Api.showTracks(tracks.data);
 		});
 		deleteTrack.appendChild(Api.createIcon("delete"));
 		deleteTrack.appendChild(Api.createElement("p", { innerHTML: "Remove song" }));
