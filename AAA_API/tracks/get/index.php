24f0c947887d075d274532ee1f15103bd80eb3fb
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-15 14:10:58 +0200

    Removed now superfluous code

--- AAA_API/tracks/get/index.php
+++ AAA_API/tracks/get/index.php
@@ -1,12 +1,5 @@
 <?php
 
-$ALLOWED_METHOD = "GET";
-$REQUIRE_TOKEN = TRUE;
-
-include_once("../../private/include_all.php");
-
-
-
 // If track ID is set, get from ID
 if(isset($_GET["id"])) {
 	$res = ITrack::findBySpotifyId($_GET["id"]);
