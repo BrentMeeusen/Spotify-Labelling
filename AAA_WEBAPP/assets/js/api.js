8dbd9cc434989d097ba227306fc6cdbf56cedb5c
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-15 13:01:51 +0200

    Updated dynamic showing of importable playlists

--- AAA_WEBAPP/assets/js/api.js
+++ AAA_WEBAPP/assets/js/api.js
@@ -175,21 +175,23 @@ Api.showPlaylistsForImport = async (playlists) => {
 
 	for(const list of playlists) {
 
-		// // Create row, add name, number of tracks, import button
-		// const row = Api.createElement("tr");
-		// row.appendChild(Api.createElement("td", { innerHTML: list.name }));
-		// row.appendChild(Api.createElement("td", { innerHTML: list.numTracks + " song" + (list.numTracks === 1 ? "" : "s") }));
+		// Create row, add name, number of tracks, import button
+		const row = Api.createElement("div", { classList: "row" });
 
-		// // If the number of tracks is more than 2000, disable button
-		// if(list.numTracks > 2000) {
-		// 	row.appendChild(Api.createIcon("import", async() => { Popup.show("Cannot import playlists with more than 2000 songs.", "error"); }));
-		// }
-		// else {
-		// 	row.appendChild(Api.createIcon("import", async () => { Api.request("api/v1/spotify/import/" + list.spotifyID, "POST"); }));
-		// }
+		const textContainer = Api.createElement("div", { classList: "text" });
+		textContainer.appendChild(Api.createElement("p", { innerHTML: list.name }));
+		textContainer.appendChild(Api.createElement("p", { innerHTML: list.numTracks + " song" + (list.numTracks === 1 ? "" : "s") }));
+		row.appendChild(textContainer);
 
-		// // Output the row
-		// output.appendChild(row);
+		// If the number of tracks is more than 2000, disable button
+		if(list.numTracks > 2000) {
+			row.appendChild(Api.createIcon("import", async() => { Popup.show("Cannot import playlists with more than 2000 songs.", "error"); }));
+		}
+		else {
+			row.appendChild(Api.createIcon("import", async () => { Api.request("api/v1/spotify/import/" + list.spotifyID, "POST"); }));
+		}
+
+		output.appendChild(row);
 
 	}
 
