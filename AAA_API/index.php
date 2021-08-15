9129295fe08667949b3153876f7381aabbd272a6
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-15 14:35:45 +0200

    Removed unused file

--- AAA_API/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-
-print(json_encode([ "your general request", "GET" => $_GET, "POST" => $_POST, "FILES" => $_FILES ]));
-
-?>
