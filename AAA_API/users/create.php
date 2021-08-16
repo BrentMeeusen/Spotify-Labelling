a2eca92947a90e36debba4f5477dd68c212f06cb
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-16 22:55:15 +0200

    Updated create endpoint

--- AAA_API/users/create.php
+++ AAA_API/users/create.php
@@ -1,20 +1,5 @@
 <?php
 
-$ALLOWED_METHOD = "POST";
-
-include_once("../../private/include_all.php");
-
-
-
-// Get the JWT from the Authorization header
-$headerJWT = (isset(getallheaders()["Authorization"]) ? @explode("Bearer ", getallheaders()["Authorization"])[1] : "");
-
-// Verify the token and get the payload if it's valid
-JSONWebToken::validateToken($headerJWT);
-$payload = JSONWebToken::getPayload($headerJWT);
-
-
-
 // If the payload doesn't contain "register", return an error
 if(!isset($payload->rights->register) || $payload->rights->register !== TRUE) {
 	ApiResponse::httpResponse(401, ["error" => "You are not allowed to register an account."]);
@@ -23,12 +8,12 @@ if(!isset($payload->rights->register) || $payload->rights->register !== TRUE) {
 
 
 // Check whether all required fields are filled in
-if(empty($body["Password"]) || empty($body["EmailAddress"])) {
+if(empty($password) || empty($email)) {
 	ApiResponse::httpResponse(400, ["error" => "Not all required fields were filled in."]);
 }
 
 // Check if the password is the same as another value
-if($body["Password"] == $body["EmailAddress"]) {
+if($password == $email) {
 	ApiResponse::httpResponse(400, ["error" => "Your password must be a unique value."]);
 }
 
