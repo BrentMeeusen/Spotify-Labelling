67292803bdecb2560172171fc4d586ca21776900
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-17 00:14:19 +0200

    Updated endpoint

--- AAA_API/labels/create.php
+++ AAA_API/labels/create.php
@@ -1,37 +1,30 @@
 <?php
 
-$ALLOWED_METHOD = "POST";
-$REQUIRE_TOKEN = TRUE;
-
-include_once("../../private/include_all.php");
-
-
-
 // Check whether the current user (JWT) is allowed to create a label
 if(!isset($payload->rights->label->create) || $payload->rights->label->create !== TRUE) {
 	ApiResponse::httpResponse(401, ["error" => "You are not allowed to create a label."]);
 }
 
 // If the label name is empty, return an error
-if(setAndEmpty($body, "Name")) {
+if(!my_isset($name)) {
 	ApiResponse::httpResponse(400, ["error" => "You have to give the label a name."]);
 }
 
 // If a label already exists, return an error
-if(Label::findByName($body["Name"], $payload->user->id) !== NULL) {
-	ApiResponse::httpResponse(400, ["error" => "You already have a label with the name \"" . $body["Name"] . "\"."]);
+if(Label::findByName($name, $payload->user->id) !== NULL) {
+	ApiResponse::httpResponse(400, ["error" => "You already have a label with the name \"$name\"."]);
 }
 
 
 
 // Set values of the payload
-$values = ["Name" => NULL, "Creator" => $payload->user->id];
+$newValues = ["Creator" => $payload->user->id];
 foreach($body as $key => $value) {
-	$values[$key] = $value;
+	$newValues[$key] = $value;
 }
 
 // Create the label
-$res = Label::create($values);
+$res = Label::create($newValues);
 
 // Properly return the results
 ApiResponse::httpResponse(200, ["message" => "The label was successfully created.", "data" => $res]);
