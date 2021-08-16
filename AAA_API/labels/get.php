6a0bcf83e3db2b943e198a9958f88079d0022a68
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-17 00:16:30 +0200

    Updated endpoint

---
+++ AAA_API/labels/get.php
@@ -0,0 +1,31 @@
+<?php
+
+// If ID is set
+if(isset($labelID)) {
+	if(!isset($payload->rights->label->find->id) || $payload->rights->label->find->id !== TRUE) {
+		ApiResponse::httpResponse(401, ["error" => "You are not allowed to get labels by ID."]);
+	}
+	$res = Label::findByPublicID(strval($labelID));
+}
+
+// If available is set
+else if(isset($_GET["available"])) {
+	if(!isset($payload->rights->label->find->available) || $payload->rights->label->find->available !== TRUE) {
+		ApiResponse::httpResponse(401, ["error" => "You are not allowed to get all available."]);
+	}
+	$res = Label::findAvailable(strval($_GET["available"]));
+}
+
+
+
+
+
+// If there's no result, return an error
+if($res === NULL) {
+	ApiResponse::httpResponse(404, ["error" => "The requested label could not be found."]);
+}
+
+// Properly return the results
+ApiResponse::httpResponse(200, [ "message" => "Label(s) found.", "data" => $res ]);
+
+?>
