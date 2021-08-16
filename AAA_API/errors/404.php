38fc9bd353709632640a6771a2dc9e1e755b8682
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-17 00:32:33 +0200

    Removed error documents

--- AAA_API/errors/404.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once("../classes/api-response.php");
-ApiResponse::httpResponse(404, [ "error" => "Not Found", "URI" => $_SERVER["REQUEST_URI"], "URL" => $_SERVER["REDIRECT_URL"] ]);
-?>
--- AAA_API/errors/405.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once("../classes/api-response.php");
-ApiResponse::httpResponse(405, [ "error" => "Method Not Allowed", "URI" => $_SERVER["REQUEST_URI"], "URL" => $_SERVER["REDIRECT_URL"] ]);
-?>
