716a5a5115d6db97afd9e2809bfb38dfcc0aa298
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-17 01:13:09 +0200

    Added 403 Forbidden HTTP response

--- AAA_API/AAA_DEV/create-tables.php
+++ AAA_API/AAA_DEV/create-tables.php
@@ -1,5 +1,6 @@
 <?php
 
+ApiResponse::httpResponse(403, ["message" => "Forbidden"]);
 Initialise::createTables(Database::connect());
 ApiResponse::httpResponse(200, ["message" => "Created tables successfully."]);
 
