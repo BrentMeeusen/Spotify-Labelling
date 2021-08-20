f7fcd2ccf06bac32cc864dfe1507b0b449dd4360
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-20 16:00:52 +0200

    Renamed variable to match my API input

--- AAA_WEBAPP/assets/js/spotify.js
+++ AAA_WEBAPP/assets/js/spotify.js
@@ -52,7 +52,7 @@ class Track {
 	artists = [];
 	labels = [];
 	name = "";
-	dateAdded;
+	addedAt;
 	releaseDate;
 
 
@@ -64,15 +64,15 @@ class Track {
 	 * 
 	 * @param {string} name The name of the tracks
 	 * @param {array} artists An array with the artist names
-	 * @param {Date} dateAdded The date at which the track was added to the application
+	 * @param {Date} addedAt The date at which the track was added to the application
 	 * @param {Date} releaseDate The date at wich the track was released
 	 * @param {array} labels An array with all the labels connected to the track
 	 */
-	constructor(name, artists, dateAdded, releaseDate, labels = []) {
+	constructor(name, artists, addedAt, releaseDate, labels = []) {
 
 		this.name = name;
 		this.artists = artists;
-		this.dateAdded = dateAdded;
+		this.addedAt = addedAt;
 		this.releaseDate = releaseDate;
 		this.labels = labels;
 
