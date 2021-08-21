ce16c81814d459c251880f96ebe36a90a392f12c
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-21 12:50:43 +0200

    Added popup

--- AAA_WEBAPP/assets/js/option-popup.js
+++ AAA_WEBAPP/assets/js/option-popup.js
@@ -31,17 +31,43 @@ class OptionPopup {
 		this.popup.style.bottom = 0;
 		this.popup.style.height ="auto";
 
+		// Add "add label" row
+		const addLabel = Api.createElement("div", { classList: "row" });
+		addLabel.addEventListener("click", async () => {
+
+			this.close();
+			const popup = new BigPopup("Choose a label", "api/v1/tracks/add-label/[TRACK_ID]/[LABEL_IDS]/", "POST");
+			const labels = await Api.get.labels();
+
+			let i = 0;
+			for(const l of labels) {
+				const el = Api.createElement("div", { innerHTML: l.name, classList: "add-label" });
+				el.dataset.selected = "false";
+				el.addEventListener("click", () => { el.dataset.selected = (el.dataset.selected === "true" ? "false" : "true"); });
+				popup.addElement(el);
+			}
+			popup.show("Add");
+
+
+			// const res = await Api.sendRequest("api/v1/tracks/" + track.id + "/labels/" + "LABEL_ID");
+			// Popup.show(res.message || res.error, (res.code >= 200 && res.code <= 299 ? "success" : "error"), 5000);
+
+			// Api.show.tracks(await Api.get.tracks());
+
+		});
+		addLabel.appendChild(Api.createIcon("add"));
+		addLabel.appendChild(Api.createElement("p", { innerHTML: "Add labels" }));
+		this.popup.appendChild(addLabel);
+
 		// Add "delete track" row
 		const deleteTrack = Api.createElement("div", { classList: "row" });
 		deleteTrack.addEventListener("click", async () => {
 
 			this.close();
-
 			const res = await Api.sendRequest("api/v1/tracks/" + track.id + "/delete", "DELETE");
 			Popup.show(res.message || res.error, (res.code >= 200 && res.code <= 299 ? "success" : "error"), 5000);
 
-			const tracks = await Api.get.tracks();
-			Api.show.tracks(tracks);
+			Api.show.tracks(await Api.get.tracks());
 
 		});
 		deleteTrack.appendChild(Api.createIcon("delete"));
