1a05bf5a52a20559c46018470929c4b0ae4ec446
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-21 13:37:01 +0200

    Created endpoint

--- AAA_API/tracks/add-labels.php
+++ AAA_API/tracks/add-labels.php
@@ -1,5 +1,22 @@
 <?php
 
-print(json_encode([$post, $trackID, $payload]));
+// Find the track
+$track = ITrack::findBySpotifyId($trackID);
+if($track === NULL) {
+	ApiResponse::httpResponse(400, ["error" => "The track to delete from your account was not found."]);
+}
+
+// For each label
+foreach($post as $key => $value) {
+
+	// Add the label to the track
+	if(strpos($key, "label") !== FALSE) {
+		$track->addLabel($payload->user->id, $value);
+	}
+
+}
+
+// Return result
+ApiResponse::httpResponse(200, ["message" => "Successfully added labels to the track."]);
 
 ?>
