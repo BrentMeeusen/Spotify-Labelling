8886f0a8dc09086327e0f4e95aaa7af307283196
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-21 12:03:24 +0200

    Removed unnecessary whitelines

--- AAA_WEBAPP/assets/js/filter.js
+++ AAA_WEBAPP/assets/js/filter.js
@@ -1,11 +1,7 @@
 const filters = document.getElementsByClassName("filter-input");
 
 for(const f of filters) {
-
 	f.addEventListener("input", () => {
-
 		Api.show.tracks(Collection.filter(f.dataset.search, f.dataset.equality, f.value));
-
 	});
-
 }
