bb4aaf626220e9ebc81a93092648018c87bcb1e5
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-24 01:13:39 +0200

    Renamed method

--- AAA_WEBAPP/assets/js/filter.js
+++ AAA_WEBAPP/assets/js/filter.js
@@ -2,6 +2,6 @@ const filters = document.getElementsByClassName("filter-input");
 
 for(const f of filters) {
 	f.addEventListener("input", () => {
-		Api.show.tracks(Collection.filter(f.dataset.search, f.value));
+		Api.show.tracks(Collection.addFilter(f.dataset.search, f.value));
 	});
 }
