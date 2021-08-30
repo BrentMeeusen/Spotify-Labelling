fa3fae1c95bb78af6b64bb023a56da871a5deffc
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-08-30 13:24:24 +0200

    Added ordering alphabetically

--- AAA_API/classes/models/label.php
+++ AAA_API/classes/models/label.php
@@ -238,7 +238,7 @@ class Label extends Database {
 	public static function findAvailable(string $ownerID) : ?array {
 
 		// If no label is found, return NULL
-		$res = parent::find("SELECT L.* FROM LABELS AS L JOIN LABELS_TO_USERS AS LTU ON L.PublicID = LTU.LabelID WHERE OwnerID = ?;", $ownerID);
+		$res = parent::find("SELECT L.* FROM LABELS AS L JOIN LABELS_TO_USERS AS LTU ON L.PublicID = LTU.LabelID WHERE OwnerID = ? ORDER BY L.Name ASC;", $ownerID);
 		if(count($res) === 0) {
 			return NULL;
 		}
