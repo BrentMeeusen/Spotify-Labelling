77044681d19a4e6e5101c9bf111ad3588b25e6e4
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-09-11 01:03:18 +0200

    Created file

---
+++ AAA_API/users/set-spotify-email.php
@@ -0,0 +1,17 @@
+<?php
+
+// If the user isn't found, return an error
+$user = User::findByPublicID($payload->user->id);
+if($user === NULL) {
+	ApiResponse::httpResponse(404, ["error" => "We couldn't find your account."]);
+}
+
+// Set the Spotify email
+SpotifyApi::setAuthorisationToken($payload->user->accessToken);
+$user->setSpotifyEmail();
+
+// Return
+ApiResponse::httpResponse(200, ["message" => "Your password was reset successfully."]);
+
+
+?>
