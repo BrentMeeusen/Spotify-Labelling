7169aece86352c9e459f4a2646406ee4842b248f
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-09-10 23:35:05 +0200

    Created functionality for forgot password button

--- AAA_WEBAPP/login/index.php
+++ AAA_WEBAPP/login/index.php
@@ -21,9 +21,13 @@ session_start();
 
 		<div class="main-wrapper">
 
+			<!-- Notification popup -->
 			<div class="popup" id="popup">
 				<p id="popup-text"></p>
 			</div>
+
+			<!-- Popup -->
+			<div class="popup-big" id="popup-big"></div>
 		
 			<h1><a href="../">Spotify Labelling</a></h1>
 
@@ -59,6 +63,16 @@ session_start();
 		?>
 
 		<script>
+
+		// Add functionality for "forgot password" link
+		document.getElementById("forgot-password").addEventListener("click", () => {
+
+			const popup = new BigPopup("Forgot password?", "api/v1/user/forgot-password", "POST");
+			popup.add("p", "", { innerHTML: "This will send an email to the email address you fill in with a link where you can reset your password." });
+			popup.add("input", "email-address", { placeholder: "Email address" });
+			popup.show("Send email");
+
+		});
 		
 		// Create a token
 		window.addEventListener("load", async () => {
