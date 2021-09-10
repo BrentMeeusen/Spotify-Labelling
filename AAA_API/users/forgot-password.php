09d2078152a9b2356779bacc0b6fc920ec07b7d4
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-09-10 23:40:42 +0200

    Created file

---
+++ AAA_API/users/forgot-password.php
@@ -0,0 +1,18 @@
+<?php
+
+// SENDS AN EMAIL TO THE USER TO RESET THE PASSWORD, DOES NOT DO THE ACTUAL RESETTING
+
+// If the user isn't found, return an error
+$user = User::findByEmailAddress($emailAddress);
+if($user === NULL) {
+	ApiResponse::httpResponse(404, ["error" => "We couldn't find your account.", "data" => $payload->user]);
+}
+
+// Send the email
+$user->requestNewPassword();
+
+// Return
+ApiResponse::httpResponse(200, ["message" => "Email sent."]);
+
+
+?>
