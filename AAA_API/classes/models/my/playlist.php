18c02d5c5dd6e6ad4f7fb7415ebcd946f89d343a
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-09-11 13:35:23 +0200

    Added create() and findByName() methods

--- AAA_API/classes/models/my/playlist.php
+++ AAA_API/classes/models/my/playlist.php
@@ -27,6 +27,72 @@ class IPlaylist {
 
 	}
 
+
+
+
+
+
+
+
+
+
+	/**
+	 * Create the playlist with the given values
+	 * 
+	 * @param		array		The values to create the playlist with
+	 * @return		IPlaylist	The playlist that was created
+	 */
+	public static function create(array $values) : IPlaylist {
+
+		// Create a playlist object
+		$playlist = new IPlaylist(Database::generateRandomID("PLAYLISTS"), $values["Name"], $values["Creator"]);
+
+		// Create playlist
+		$stmt = self::prepare("INSERT INTO PLAYLISTS (PublicID, Name) VALUES (?, ?);");
+		$playlist->sanitizeInputs();
+		$stmt->bind_param("ss", $playlist->publicID, $playlist->name);
+		self::execute($stmt);
+
+		// Create playlist-to-user
+		$stmt = self::prepare("INSERT INTO PLAYLISTS_TO_USERS (PlaylistID, UserID) VALUES (?, ?);");
+		$stmt->bind_param("ss", $playlist->publicID, $playlist->creator);
+		self::execute($stmt);
+
+		// Return the result
+		return $playlist;
+
+	}
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * Finds the playlist by name
+	 * 
+	 * @param		string		The name of the playlist
+	 * @param		string		The owner ID
+	 * @return		IPlaylist	If it was found
+	 * @return		null		If no playlist was found
+	 */
+	public static function findByName(string $name, string $userID) : ?IPlaylist {
+
+		// If no playlist is found, return NULL
+		$res = parent::findLink("SELECT P.* FROM PLAYLISTS AS P JOIN PLAYLISTS_TO_USERS AS PTU ON P.PublicID = PTU.PlaylistID WHERE Name = ? AND PTU.UserID = ?;", $name, $userID);
+		if(count($res) === 0) {
+			return NULL;
+		}
+
+		// Create and return the found playlist as an object
+		return new IPlaylist($res[0]->PublicID, $res[0]->Name, $userID);
+
+	}
+
 }
 
 
