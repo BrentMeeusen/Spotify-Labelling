61a24722ab1ac4f726fb277bacef3ce741c8b008
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-09-17 21:48:39 +0200

    Updated logics

--- AAA_API/playlists/delete.php
+++ AAA_API/playlists/delete.php
@@ -1,52 +1,21 @@
 <?php
 
-// If the ID is set, delete ID
-if(isset($id)) {
-
-	// Check whether the current user (JWT) is allowed to delete another user (ID)
-	if(!isset($payload->rights->users->delete) || $payload->rights->users->delete !== TRUE) {
-		ApiResponse::httpResponse(401, ["error" => "You are not allowed to delete someone else's account."]);
-	}
-	$userID = $id;
-
-	$prefix = "The";
-
+// If ID is not set, return an error
+if(!isset($id)) {
+	ApiResponse::httpResponse(400, ["error" => "The playlist to delete was not found."]);
 }
 
-// If the ID is not set, delete self
-else {
-
-	// If the payload doesn't contain "user.id", return an error
-	if(!isset($payload->user->id) || !isset($payload->rights->user->delete) || $payload->rights->user->delete !== TRUE) {
-		ApiResponse::httpResponse(401, ["error" => "You are not allowed to delete your account."]);
-	}
-	$userID = $payload->user->id;
-	
-	$prefix = "Your";
-
+// If the playlist is not created by user, return an error
+$playlist = IPlaylist::findById($id);
+if($playlist === NULL) {
+	ApiResponse::httpResponse(404, ["error" => "We couldn't find the playlist.", "data" => $payload->user]);
 }
 
-
-
-// If the user isn't found, return an error
-$user = User::findByPublicID($userID);
-if($user === NULL) {
-	ApiResponse::httpResponse(404, ["error" => "$prefix account was not found."]);
-}
-
-
-// If the password isn't correct, return an error
-if(!password_verify($password, $user->password)) {
-	ApiResponse::httpResponse(400, ["error" => "The given password is incorrect."]);
-}
-
-
-
-// Delete the user
-$res = User::delete($user);
+// Delete the playlist
+$res = IPlaylist::delete($playlist);
 
 // Properly return the results
-ApiResponse::httpResponse(200, ["message" => "$prefix account has been successfully deleted.", "data" => $res]);
+ApiResponse::httpResponse(200, ["message" => "The playlist has been successfully deleted.", "data" => $res]);
 
 
 ?>
