7014da088cf18223019f30b6ed0dd6ab0e32eef2
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-09-17 23:36:19 +0200

    Updated scope

--- AAA_WEBAPP/assets/php/auth.php
+++ AAA_WEBAPP/assets/php/auth.php
@@ -1,4 +1,4 @@
 <?php
 // WARNING: HARDCODED; Paste this: redirect_uri=http%3A%2F%2Fspotify-labelling.21webb.nl%2Fassets%2Fphp%2Fget-access-code.php&
-header("Location: https://accounts.spotify.com/authorize?response_type=code&client_id=a209cbda1aaa4f408bd6ae2efc2264fb&redirect_uri=http%3A%2F%2Flocalhost%2FSpotify%20Labelling%2FAAA_WEBAPP%2Fassets%2Fphp%2Fget-access-code.php&scope=user-read-email%20user-read-private%20playlist-modify-public%20playlist-modify-private%20playlist-read-private%20playlist-read-collaborative%20user-library-read&show_dialog=false");
+header("Location: https://accounts.spotify.com/authorize?response_type=code&client_id=a209cbda1aaa4f408bd6ae2efc2264fb&redirect_uri=http%3A%2F%2Flocalhost%2FSpotify%20Labelling%2FAAA_WEBAPP%2Fassets%2Fphp%2Fget-access-code.php&scope=user-read-email%20playlist-modify-public%20playlist-modify-private%20playlist-read-private%20playlist-read-collaborative%20user-library-read&show_dialog=false");
 ?>
