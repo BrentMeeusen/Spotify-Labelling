49e51cd7d309b8f76c7b7c4766b64777ae9879e6
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-09-17 22:46:51 +0200

    Made method work with multiple labels to remove

--- AAA_API/tracks/remove-label.php
+++ AAA_API/tracks/remove-label.php
@@ -1,13 +1,37 @@
 <?php
 
-// Find the track
-$track = ITrack::findBySpotifyId($trackID);
-if($track === NULL) {
-	ApiResponse::httpResponse(400, ["error" => "The track to remove a label from was not found."]);
+$tracks = [];
+
+// If it's only one track
+if(!(empty($trackID))) {
+	$tracks = [ITrack::findBySpotifyId($trackID)];
+	if($tracks[0] === NULL) {
+		ApiResponse::httpResponse(400, ["error" => "The track to remove a label from was not found."]);
+	}
+}
+// If it's multiple tracks
+else if(!(empty($post->tracks))) {
+	foreach($post->tracks as $track) {
+		array_push($tracks, ITrack::findBySpotifyId($track));
+	}
 }
 
-// Remove the label
-$track->removeLabel($labelID);
+
+
+// For each track
+foreach($tracks as $track) {
+
+	// For each label
+	foreach($post as $key => $value) {
+
+		// Remove the label from the track
+		if(strpos($key, "label") !== FALSE) {
+			$track->removeLabel($value);
+		}
+
+	}
+
+}
 
 // Return result
 ApiResponse::httpResponse(200, ["message" => "Successfully removed the label from the track."]);
