df68ab7fe52c0509cf87325071a60acd4b6e24c6
Brent Meeusen <b.a.j.meeusen@student.tudelft.nl>
2021-09-17 21:58:21 +0200

    Removed return statement from void

--- AAA_API/classes/models/my/playlist.php
+++ AAA_API/classes/models/my/playlist.php
@@ -84,10 +84,7 @@ class IPlaylist extends Database {
 	 * @param		IPlaylist	The playlist to delete
 	 */
 	public static function delete(IPlaylist $playlist) : void {
-
-		// Delete the entry
-		return parent::deleteEntry($label, "PLAYLISTS");
-
+		parent::deleteEntry($label, "PLAYLISTS");
 	}
 
 
